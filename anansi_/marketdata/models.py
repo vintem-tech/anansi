9d25a41a88375b2fa61b24651eb48e55e74a3533
marcusmello <mandealista@gmail.com>
2020-12-20 15:43:01 -0300

    Just comment modification

--- anansi_/marketdata/models.py
+++ anansi_/marketdata/models.py
@@ -1,14 +1,12 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=no-self-argument
 
-# from typing import Optional
-
 """ Classes que abstraem dados que transitam entre partes diferentes do
 sistema.
 """
 
 from pydantic import BaseModel
-
+#from typing import Sequence
 
 class Market(BaseModel):
     """ Market attributes """
