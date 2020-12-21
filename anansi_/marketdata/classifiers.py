5fdc8c53a81dffd0ab45dd018b0ff9bca71deb67
marcusmello <mandealista@gmail.com>
2020-12-21 17:04:56 -0300

    End of 'DidiIndexAlpha' implementation

--- anansi_/marketdata/classifiers.py
+++ anansi_/marketdata/classifiers.py
@@ -1,28 +1,26 @@
 import sys
-from .models import Market
-from .setups import SmaTripleCrossSetup, FollowBullsSetup
+from .models import BaseModel, Market, SmaTripleCross
 from .klines import klines_getter
 import pandas as pd
 from typing import Union
 
-thismodule = sys.modules[__name__]
+# thismodule = sys.modules[__name__]
 
-
-def classifier(
-    market: Market,
-    setup: Union[
-        FollowBullsSetup,
-    ],
-    time_frame: str,
-):
-    classifier_name = (setup.__class__.__name__).split("Setup")[0]
-    return getattr(thismodule, classifier_name)(
-        market, setup, time_frame
-    )
+# def classifier(
+#    market: Market,
+#    setup: Union[
+#        FollowBullsSetup,
+#    ],
+#    time_frame: str,
+# ):
+#    classifier_name = (setup.__class__.__name__).split("Setup")[0]
+#    return getattr(thismodule, classifier_name)(
+#        market, setup, time_frame
+#    )
 
 
 def cross_triple_sma(
-    setup: SmaTripleCrossSetup, data: pd.core.frame.DataFrame
+    setup: SmaTripleCross, data: pd.core.frame.DataFrame
 ) -> None:
     _setup = type("", (), {})()
     _setup.price_metrics = setup.price_metrics
@@ -32,37 +30,102 @@ def cross_triple_sma(
         data.apply_indicator.trend.simple_moving_average(_setup)
 
 
-class FollowBulls:
-    def __init__(
-        self, market: Market, setup: FollowBullsSetup, time_frame: str
-    ):
+class DidiIndexAlpha:
+    def __init__(self, market: Market, setup: BaseModel, time_frame: str):
         self.backtesting = False
         self.result = pd.DataFrame()
         self.market = market
         self.setup = setup
         self.time_frame = time_frame
-        self.number_samples = max(setup.Trend.sma_triple_cross.number_samples)
+        self.number_samples = max(setup.simple_moving_average.number_samples)
 
-    def _indicators_pipeline(self):
-        # Trend indicators
-        cross_triple_sma(
-            setup=self.setup.Trend.sma_triple_cross,
-            data=self.data,
+    def _trend_analysis(self):
+        small_sma, middle_sma, large_sma = (
+            getattr(self.data, "SMA_{}".format(n)).tail(1).item()
+            for n in self.setup.simple_moving_average.number_samples
         )
 
-        # Volatility indicators
+        didi_fast_sma = small_sma / middle_sma - 1
+        didi_slow_sma = large_sma / middle_sma - 1
+
+        didi_result = (
+            1
+            if (didi_fast_sma > 0 and didi_slow_sma < 0)
+            else -1
+            if (
+                didi_fast_sma < 0
+                and didi_slow_sma > 0
+                and self.setup.allow_naked_sells
+            )
+            else 0
+        )
+        self.result["Trend_result"] = didi_result
+        self.result["Didi_fast"] = didi_fast_sma
+        self.result["Did_slow"] = didi_slow_sma
+
+    def _volatility_analysis(self):
+        previous_BB_upper = self.data[-2:-1].BB_upper.item()
+        current_BB_upper = self.data[-1:].BB_upper.item()
+
+        previous_BB_lower = self.data[-2:-1].BB_lower.item()
+        current_BB_lower = self.data[-1:].BB_lower.item()
+
+        total_opened_bands = bool(
+            (current_BB_upper > previous_BB_upper)
+            and (current_BB_lower < previous_BB_lower)
+        )
+        partial_opened_bands = bool(
+            (
+                (current_BB_upper > previous_BB_upper)
+                and (current_BB_lower >= previous_BB_lower)
+            )
+            or (
+                (current_BB_upper <= previous_BB_upper)
+                and (current_BB_lower < previous_BB_lower)
+            )
+        )
+        bollinger_result = (
+            1 if total_opened_bands else 0.5 if partial_opened_bands else 0
+        )
+        self.result["Bollinger_result"] = bollinger_result
+
+    def _indicators_pipeline(self):
+        cross_triple_sma(
+            setup=self.setup.simple_moving_average,
+            data=self.data,
+        )
         self.data.apply_indicator.volatility.bollinger_bands(
-            setup=self.setup.Volatility.bollinger_bands
+            setup=self.setup.bollinger_bands
         )
 
     def _analysis_of_indicators(self):
-        self.result = self.data
+        self.result = pd.DataFrame()
+        self.result = self.result.append(self.data[-1:], ignore_index=True)
 
-    def restult_at(self, timestamp: int) -> pd.core.frame.DataFrame:
+        self._trend_analysis()
+        self._volatility_analysis()
+
+        leverage = (
+            self.result.Trend_result.item()
+            * self.result.Bollinger_result.item()
+        )
+        suggest_side = (
+            "Long"
+            if leverage > 0.3
+            else "Short"
+            if leverage < -0.3
+            else "Zeroed"
+        )
+        self.result["Suggest_side"] = suggest_side
+        self.result["Leverage"] = leverage
+
+    def restult_at(
+        self, desired_datetime: Union[str, int]
+    ) -> pd.core.frame.DataFrame:
         klines = klines_getter(self.market, self.time_frame, self.backtesting)
 
         self.data = klines.get(
-            until=timestamp, number_of_candles=self.number_samples
+            until=desired_datetime, number_of_candles=self.number_samples
         )
         self._indicators_pipeline()
         self._analysis_of_indicators()
