0753ca0a8307c79197c66b6f8e3a7ef3cb4e8091
marcusmello <mandealista@gmail.com>
2020-12-28 02:32:13 -0300

    Notifiers and real trading

--- anansi_/trading/main.py
+++ anansi_/trading/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import pendulum
-from .schemas import OpSetup
+import time
+from .schemas import OpSetup, BaseModel
 from .classifiers import classifier
 from .order_handler import order_handler
 from ..share.tools import short_hash_from
 from ..share.storage import StorageResults
+from ..share.notifiers import Notifier
 
 
 class Operation:
@@ -34,15 +36,19 @@ class DefaultTrader:
         self.order_handler = order_handler(operation)
         self.is_running: bool = False
         self.now: int = None  # Datetime (seconds UTC timestamp)
+        self.notifier = Notifier(broadcasters=operation.setup.broadcasters)
+        self.notifier.show_header = False
 
     def _start(self):
+        debbug = self.operation.setup.debbug
         self.now = (
             self.classifier.initial_backtesting_now()
             if self.operation.setup.backtesting
             else pendulum.now("UTC").int_timestamp
         )
         self.is_running = True
-
+        if debbug: self.notifier.debbug(msg="Starting Anansi")
+        
     def _classifier_analysis(self):
         result = self.classifier.restult_at(desired_datetime=self.now)
         self.operation.update(result)
@@ -51,15 +57,24 @@ class DefaultTrader:
         self._classifier_analysis()
         self.order_handler.proceed()
 
+    # TODO: Timesleep para o trade real
     def forward_step(self):
         if self.operation.setup.backtesting:
             self.now += self.classifier.time_frame_total_seconds
 
             if self.now > self.classifier.final_backtesting_now():
                 self.is_running = False
+        
+        else:
+            sleep_time = self.classifier.time_until_next_closed_candle + 5
+            time.sleep(sleep_time)
 
     def run(self):
         self._start()
         while self.is_running:
-            self.analysis_pipeline()
-            self.forward_step()
+            try:
+                self.analysis_pipeline()
+                self.forward_step()
+            
+            except Exception as e:
+                self.notifier.error(e)
