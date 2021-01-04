494a7f0a08e409d0efff85f148ac588def211f9b
marcusmello <mandealista@gmail.com>
2021-01-04 20:48:37 -0300

    Change import flow due to refactoring

--- app/anansi_/trading/main.py
+++ app/anansi_/trading/main.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import pendulum
 import time
-from .schemas import OpSetup, BaseModel
-from .classifiers import classifier
-from .order_handler import order_handler
-from ..share.tools import short_hash_from
-from ..share.storage import StorageResults
-from ..share.notifiers import Notifier
+from ..sql_app.schemas import OpSetup  # , BaseModel
+from .classifiers import get_classifier
+from .order_handler import get_order_handler
+from ..tools.hashes import hash_from_an_object
+from ..storage.storage import StorageResults
+from ..notifiers.notifiers import get_notifier
 
 
 class Operation:
     def __init__(self, setup: OpSetup):
         self.setup = setup
-        self.id = short_hash_from(setup)
+        self.id = hash_from_an_object(setup)
         self.current_result = pd.DataFrame()
         self.storage = StorageResults(table=self.id, database="results")
 
@@ -32,12 +32,13 @@ class Operation:
 class DefaultTrader:
     def __init__(self, operation: Operation):
         self.operation = operation
-        self.classifier = classifier(**operation.setup.classifier_payload)
-        self.order_handler = order_handler(operation)
+
+        self.classifier = get_classifier(**operation.setup.classifier_payload)
+        self.notifier = get_notifier(broadcasters=operation.setup.broadcasters)
+        self.order_handler = get_order_handler(operation)
+
         self.is_running: bool = False
         self.now: int = None  # Datetime (seconds UTC timestamp)
-        self.notifier = Notifier(broadcasters=operation.setup.broadcasters)
-        self.notifier.show_header = False
 
     def _start(self):
         debbug = self.operation.setup.debbug
@@ -47,8 +48,9 @@ class DefaultTrader:
             else pendulum.now("UTC").int_timestamp
         )
         self.is_running = True
-        if debbug: self.notifier.debbug(msg="Starting Anansi")
-        
+        if debbug:
+            self.notifier.debbug(msg="Starting Anansi")
+
     def _classifier_analysis(self):
         result = self.classifier.restult_at(desired_datetime=self.now)
         self.operation.update(result)
@@ -57,14 +59,13 @@ class DefaultTrader:
         self._classifier_analysis()
         self.order_handler.proceed()
 
-    # TODO: Timesleep para o trade real
     def forward_step(self):
         if self.operation.setup.backtesting:
             self.now += self.classifier.time_frame_total_seconds
 
             if self.now > self.classifier.final_backtesting_now():
                 self.is_running = False
-        
+
         else:
             sleep_time = self.classifier.time_until_next_closed_candle + 5
             time.sleep(sleep_time)
@@ -72,9 +73,9 @@ class DefaultTrader:
     def run(self):
         self._start()
         while self.is_running:
-            try:
-                self.analysis_pipeline()
-                self.forward_step()
-            
-            except Exception as e:
-                self.notifier.error(e)
+            #try:
+            self.analysis_pipeline()
+            self.forward_step()
+
+            #except Exception as e:
+            #    self.notifier.error(e)
