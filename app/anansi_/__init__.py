00d3218db337ba9601ad258ebd78cb3ab234efa1
marcusmello <mandealista@gmail.com>
2021-01-04 20:48:18 -0300

    No more import here

--- app/anansi_/__init__.py
+++ app/anansi_/__init__.py
@@ -1,5 +1,3 @@
-"""Anansi subpackages import """
-
-#from . import brokers, marketdata, settings, tools, sql_app, storage, trading
+"""Anansi, market analysis and trading."""
 
 __version__ = "0.1.0"
