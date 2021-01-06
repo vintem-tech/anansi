c0a4205303d93b8a7e6b09510de767199a0ecaf4
marcusmello <mandealista@gmail.com>
2021-01-06 19:48:01 -0300

    Formatting

--- app/anansi_/start.py
+++ app/anansi_/start.py
@@ -1,15 +1,10 @@
-from .trading.schemas import DefaultRealTradingOperation
+from .sql_app.schemas import DefaultRealTradingOperation
 from .trading.main import Operation, DefaultTrader
 
 my_op_setup = DefaultRealTradingOperation
-
-
-
 my_op = Operation(setup=my_op_setup)
-
-
-
 my_trader = DefaultTrader(operation=my_op)
+
 
 def run():
     my_trader.run()
