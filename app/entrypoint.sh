fdd0d9a2e3f9b6b15ffe4b4c64bbfa57fae7a92e
marcusmello <mandealista@gmail.com>
2021-01-06 19:48:08 -0300

    Netcat (nc) is working now

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-#! TODO: Deal with 'nc' dependency
-#echo "Waiting for Influx..."
+echo "Waiting for Influx..."
 
-#while ! nc -z $INFLUXDB_HOST $INFLUXDB_PORT; do
-#    echo "Influx not read"
-#    sleep 3
-#done
+while ! nc -z $INFLUXDB_HOST $INFLUXDB_PORT; do
+    echo "Influx not read"
+    sleep 3
+done
 
-#echo "Influx started"
-echo "sleeping"
-sleep 30
-echo "awaiking"
-
+echo "Influx started"
 exec poetry run anansi
