e5a833bf563648b25e75601315947e5c3f2e05ff
marcusmello <mandealista@gmail.com>
2021-01-19 20:44:09 -0300

    High level functions to operate models

---
+++ app/anansi_/trading/views.py
@@ -0,0 +1,12 @@
+from pony.orm import db_session
+from . import defaults
+from .models import Position, Operation, TradeLog, LastCheck
+
+@db_session
+def create_default_operation(operation_mode: str):
+    Operation(
+        setup=getattr(defaults, operation_mode).json(),
+        position=Position(),
+        last_check=LastCheck(by_classifier_at=0),
+        trade_log=TradeLog(),
+    )
