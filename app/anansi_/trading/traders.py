68cf88477b2fe7fb4bb4b4a9abf1af606e066fe2
marcusmello <mandealista@gmail.com>
2021-01-19 22:10:55 -0300

    Adequacy in order to get a 'setup' object

--- app/anansi_/trading/traders.py
+++ app/anansi_/trading/traders.py
@@ -12,8 +12,9 @@ from .classifiers import get_classifier
 from .order_handler import get_order_handler
 from .models import Operation
 
+
 class DefaultTrader(Thread):
-    def __init__(self, operation:Operation):
+    def __init__(self, operation: Operation):
         self.operation = operation
         self.setup = Deserialize(name="setup").from_json(operation.setup)
 
@@ -21,23 +22,25 @@ class DefaultTrader(Thread):
             classifier_name=self.setup.classifier_name,
             market=self.setup.market,
             time_frame=self.setup.time_frame,
-            setup=self.setup.classifier_setup)
+            setup=self.setup.classifier_setup,
+        )
 
-        self.notifier = get_notifier(broadcasters=operation.setup.broadcasters)
+        self.notifier = get_notifier(broadcasters=self.setup.broadcasters)
         self.order_handler = get_order_handler(operation)
         self.now: int = None  # Datetime (seconds UTC timestamp)
         super().__init__()
 
     def _start(self):
-        debbug = self.operation.setup.debbug
+        debbug = self.setup.debbug
         self.now = (
             self.classifier.initial_backtesting_now()
-            if self.operation.setup.backtesting
+            if self.setup.backtesting
             else pendulum.now("UTC").int_timestamp
         )
-        self.is_running = True
+        self.operation.update(is_running=True)
+
         if debbug:
-            setup_str = text_in_lines_from_dict(self.operation.setup.dict())
+            setup_str = text_in_lines_from_dict(self.setup._asdict())
             msg = "Starting Anansi. Your operation id is {}\n\n{}".format(
                 self.operation.id, setup_str
             )
@@ -46,25 +49,27 @@ class DefaultTrader(Thread):
     def _classifier_analysis(self):
         # TODO: Só realizar esta analise se já existir um candle aberto...
         result = self.classifier.restult_at(desired_datetime=self.now)
-        self.operation.update(result)
+        self.operation.update_current_result(result)
 
     def analysis_pipeline(self) -> None:
         self._classifier_analysis()
         self.order_handler.proceed()
 
     def forward_step(self):
-        if self.operation.setup.backtesting:
+        debbug = self.setup.debbug
+        if self.setup.backtesting:
             self.now += self.classifier.time_frame_total_seconds
 
             if self.now > self.classifier.final_backtesting_now():
-                self.is_running = False
+                self.operation.update(is_running=False)
 
         else:
-            sleep_time = self.classifier.time_until_next_closed_candle + 30
-            if self.operation.setup.debbug:
+            sleep_time = self.classifier.time_until_next_closed_candle + 10
+            if debbug:
                 utc_now = ParseDateTime(
                     (pendulum.now(tz="UTC")).int_timestamp
                 ).from_timestamp_to_human_readable()
+
                 msg = """Time now (UTC) = {}\nSleeping {} s.""".format(
                     utc_now, sleep_time
                 )
@@ -75,14 +80,14 @@ class DefaultTrader(Thread):
 
     def run(self):
         self._start()
-        while self.is_running:
-            try:
-                self.analysis_pipeline()
-                self.forward_step()
+        while self.operation.is_running:
+            #try:
+            self.analysis_pipeline()
+            self.forward_step()
 
-            except Exception as e:
-                self.notifier.error(e)
-                time.sleep(3600)  # 1 hour cooldown
+            #except Exception as e:
+                #self.notifier.error(e)
+                #time.sleep(3600)  # 1 hour cooldown
 
         final_msg = "Op. {} finalized!".format(self.operation.id)
         self.notifier.error(final_msg)
