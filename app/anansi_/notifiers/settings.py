e0e24009f95ed02b5a6ffacd3546c6091706e7dd
marcusmello <mandealista@gmail.com>
2021-01-24 16:43:48 -0300

    Default telegram parameters as empty strings

--- app/anansi_/notifiers/settings.py
+++ app/anansi_/notifiers/settings.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 env = Env()
 
 class TelegramSettings(BaseModel):
-    token:str = env.str("TELEGRAM_TOKEN", default=None)
-    debbug_id:int = env.int("TELEGRAM_DEBBUG_ID", default=None)
-    error_id:int = env.int("TELEGRAM_ERROR_ID", default=None)
-    trade_id:int = env.int("TELEGRAM_TRADE_ID", default=None)
+    token:str = env.str("TELEGRAM_TOKEN", default=str())
+    debbug_id:int = env.int("TELEGRAM_DEBBUG_ID", default=str())
+    error_id:int = env.int("TELEGRAM_ERROR_ID", default=str())
+    trade_id:int = env.int("TELEGRAM_TRADE_ID", default=str())
