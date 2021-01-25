39b7fdd1ffafe89201a8b1ad7648fdf2eb866bec
marcusmello <mandealista@gmail.com>
2021-01-25 11:06:35 -0300

    Now, each operation mode has a particular 'OperationalSetup' instantiation

--- app/anansi_/trading/defaults.py
+++ app/anansi_/trading/defaults.py
@@ -1,25 +1,45 @@
-from ..sql_app.schemas import Market, OpSetup
+from ..sql_app.schemas import Market, OperationSetup, DidiClassifierSetup
 
-back_testing = OpSetup()
-
-trading_testing = OpSetup(
-    debbug=True,
+back_testing = OperationSetup(
+    debbug = True,
+    broadcasters = ["PrintNotifier"],
+    classifier_name = "DidiClassifier",
+    classifier_setup = DidiClassifierSetup(),
+    market = Market(
+        broker_name="Binance", quote_symbol="BTC", base_symbol="USDT", ticker_symbol="BTCUSDT"
+    ),
+    backtesting = True,
+    initial_base_amount = 1000.00,
+    test_order = False,
+    order_type = "market",
+    stop_is_on = False,
+    allow_naked_sells = False,
+)
+trading_testing = OperationSetup(
+    debbug = True,
     broadcasters=["PrintNotifier", "TelegramNotifier"],
+    classifier_name = "DidiClassifier",
+    classifier_setup = DidiClassifierSetup(),
     market=Market(
         broker_name="Binance", quote_symbol="BTC", base_symbol="EUR", ticker_symbol="BTCEUR",
     ),
-    time_frame="6h",
-    backtesting=False,
-    test_order=True,
+    backtesting = False,
+    test_order = True,
+    order_type = "market",
+    stop_is_on = False,
+    allow_naked_sells = False,
 )
-
-real_trading = OpSetup(
+real_trading = OperationSetup(
     debbug=True,
     broadcasters=["TelegramNotifier"],
+    classifier_name = "DidiClassifier",
+    classifier_setup = DidiClassifierSetup(),
     market=Market(
         broker_name="Binance", quote_symbol="BTC", base_symbol="EUR", ticker_symbol="BTCEUR",
     ),
-    time_frame="6h",
     backtesting=False,
-    test_order=False,
+    test_order = False,
+    order_type = "market",
+    stop_is_on = False,
+    allow_naked_sells = False,
 )
