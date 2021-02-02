acf495b9e564941b5b403161ade6c681c5b71077
marcusmello <mandealista@gmail.com>
2021-02-02 19:49:14 -0300

    Started total refactoring

--- app/anansi_/trading/order_handler.py
+++ app/anansi_/trading/order_handler.py
@@ -1,6 +1,7 @@
 # pylint: disable=E1136
 # pylint: disable=no-name-in-module
 
+import pandas as pd
 import sys
 from typing import Optional, Union
 
@@ -9,7 +10,11 @@ from pydantic import BaseModel
 from .brokers import get_broker
 from ..marketdata.klines import PriceFromStorage
 from ..notifiers.notifiers import get_notifier
-from ..sql_app.schemas import Order, Portfolio  # Market
+from ..sql_app.schemas import (
+    # Order,
+    Portfolio,
+    PossibleSignals as sig,
+)  # Market
 from ..tools.serializers import Deserialize
 from ..tools.time_handlers import ParseDateTime
 
@@ -17,369 +22,142 @@ from .models import Operation
 
 thismodule = sys.modules[__name__]
 
+
+class Order(BaseModel):
+    """Order parameters collection"""
+
+    test_order: bool = False
+    notify: bool = True
+    signal: Optional[str]
+    order_type: Optional[str] = "market"
+    quantity: Optional[float]
+    leverage: Optional[float]
+    price: Optional[float] = None
+    at_time: Optional[int]
+    portfolio: Optional[Portfolio]
+    fee: Optional[float]
+
+
 class Signal:
     def __init__(self, from_side: str, to_side: str, by_stop: bool):
         self.from_side = from_side.capitalize()
         self.to_side = to_side.capitalize()
         self.by_stop = by_stop
 
-    def get(self):
+    def generate(self):
         if self.from_side == self.to_side:
-            return "HOLD"
+            return sig.hold
 
         if self.from_side == "Zeroed":
             if self.to_side == "Long":
-                return "BUY"
+                return sig.buy
             if self.to_side == "Short":
-                return "NAKED_SELL"
+                return sig.naked_sell
 
         if self.from_side == "Long":
             if self.to_side == "Zeroed":
                 if self.by_stop:
-                    return "LONG_STOPPED"
-                return "SELL"
+                    return sig.long_stopped
+                return sig.sell
             if self.to_side == "Short":
-                return "DOUBLE_NAKED_SELL"
+                return sig.double_naked_sell
 
         if self.from_side == "Short":
             if self.to_side == "Zeroed":
                 if self.by_stop:
-                    return "SHORT_STOPPED"
-                return "BUY"
+                    return sig.short_stopped
+                return sig.buy
             if self.to_side == "Long":
-                return "DOUBLE_SELL"
+                return sig.double_buy
+
+
+class BackTestingBroker:
+    def __init__(self, operation):
+        self.operation = operation
+
+    def get_price(self) -> float:
+        """Instant average trading price"""
+        raise NotImplementedError
+
+    def get_portfolio(self) -> Portfolio:
+        """The portfolio composition, given a market"""
+        raise NotImplementedError
+
+    def get_min_lot_size(self) -> float:
+        """Minimal possible trading amount, by quote."""
+        raise NotImplementedError
+
+    def execute_order(self, order: Order):
+        """Proceed trade orders """
+        raise NotImplementedError
+
+    def test_order(self, order: Order):
+        """Proceed test orders """
+        raise NotImplementedError
+
+    def check_order(self, order_id):
+        """Verify order status and return the relevant informations"""
+        raise NotImplementedError
+
 
 class OrderHandler:
-    def __init__(self, operation:Operation):
-        self.operation = operation
-    
-    def proceed(self, at_time: int, from_side: str, to_side: str, by_stop: bool=False):
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Parameters(BaseModel):
-    quote_symbol: Optional[str]
-    base_symbol: Optional[str]
-    initial_equivalent_quote: Optional[float]
-    min_lot_size: Optional[float]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class BkpOrderHandler:
     def __init__(self, operation: Operation):
         self.operation = operation
         self.setup = Deserialize(name="setup").from_json(operation.setup)
-        self.broker = get_broker(self.setup.market)
-        self.notifier = get_notifier(broadcasters=self.setup.broadcasters)
-        self.parameters = self._fill_in_parameters()
-
-    def _fill_in_parameters(self):
-        parameters = Parameters()
-
-        parameters.quote_symbol = self.setup.market.quote_symbol
-        parameters.base_symbol = self.setup.market.base_symbol
-        parameters.initial_equivalent_quote: float = None
-        parameters.min_lot_size = self.broker.get_min_lot_size()
-
-        return parameters
-
-    def current_price(self):
-        raise NotImplementedError
-
-    def update_result(self, portfolio: Portfolio, price) -> None:
-        backtesting = bool(self.setup.backtesting)
-        debbug = bool(self.setup.debbug)
-        result = self.operation.current_result
-        equivalent_base = portfolio.quote * price + portfolio.base
-
-        result[self.parameters.quote_symbol] = portfolio.quote
-        result[self.parameters.base_symbol] = portfolio.base
-
-        result[
-            "Equivalent_{}".format(self.parameters.base_symbol)
-        ] = equivalent_base
-        result["Price"] = price
-        if backtesting:
-            result["Hold"] = self.parameters.initial_equivalent_quote * price
-
-        if debbug:
-            current_opentime = result.Open_time.tail(1).item()
-            side = result.Side.tail(1).item()
-            leverage = result.Leverage.tail(1).item()
-
-            msg = """\n{}
-===================\n
-Equivalent {} = {} 
-Price = {} 
-Quote amount = {} {}
-Base amount = {} {}\n
-Side = {}
-Leverage = {}
-""".format(
-                current_opentime,
-                self.parameters.base_symbol,
-                equivalent_base,
-                price,
-                portfolio.quote,
-                self.parameters.quote_symbol,
-                portfolio.base,
-                self.parameters.base_symbol,
-                side,
-                leverage,
-            )
-            self.notifier.debbug(msg)
-        self.operation.save_current_result()
-
-    def get_portfolio(self) -> Portfolio:
-        raise NotImplementedError
-
-    def proceed(self):
-        raise NotImplementedError
-
-
-class BacktestingOrderHandler(OrderHandler):
-    def __init__(self, operation: Operation):
-        super().__init__(operation)
-
-    def current_price(self) -> float:
-        price_getter = PriceFromStorage(self.setup.market, "ohlc4")
-        current_time = self.operation.current_result.Open_time.tail(1).item()
-        return price_getter.get_price_at(desired_datetime=current_time)
-
-    def _reset_result(self):
-        portfolio = Portfolio()
-        initial_base_amount = self.setup.initial_base_amount
-        price = self.current_price()
-
-        self.parameters.initial_equivalent_quote = initial_base_amount / price
-
-        portfolio.quote = 0.0
-        portfolio.base = initial_base_amount
-
-        self.update_result(portfolio, price=price)
-
-    def _get_asset(self, asset_kind: str) -> float:
-        asset_key = getattr(self.parameters, "{}_symbol".format(asset_kind))
-        return (self.operation.current_result[asset_key]).item()
-
-    def get_portfolio(self) -> Portfolio:
-        portfolio = Portfolio()
-
-        portfolio_does_not_exist = not bool(
-            (self.parameters.quote_symbol and self.parameters.base_symbol)
-            in self.operation.current_result.columns
+        self.broker = (
+            BackTestingBroker(operation)
+            if self.setup.backtesting
+            else get_broker(self.setup.market)
         )
-        if portfolio_does_not_exist:
-            self._reset_result()
+        self.order = Order()
 
-        portfolio.quote = self._get_asset("quote")
-        portfolio.base = self._get_asset("base")
-
-        return portfolio
-
-    def proceed(self):
-        result = self.operation.current_result
-        price = self.current_price()
-        portfolio = self.get_portfolio()
-        side = result.Side.item()
-        leverage = result.Leverage.item()
-        fee_rate_decimal = self.broker.fee_rate_decimal
-        opentime = ParseDateTime(
-            result.Open_time.tail(1).item()
-        ).from_human_readable_to_timestamp()
-
-        if not self.setup.allow_naked_sells and side == "Short":
-            side = "Zeroed"
-
-        if side == "Long":
-            order_amount = leverage * (portfolio.base / price)
-
-            if order_amount > self.parameters.min_lot_size:
-                fee_quote = fee_rate_decimal * order_amount
-                bought_quote_amount = order_amount - fee_quote
-
-                portfolio.quote += bought_quote_amount
-                portfolio.base -= order_amount * price
-
-                order_dict = dict(
-                    timestamp=opentime,
-                    signal="BUY",
-                    price=price,
-                    fee=price * fee_quote,
-                    quote_amount=order_amount,
-                )
-                self.operation.report_trade(order_dict)
-                self.operation.position.update(
-                    side="Long",
-                    enter_price=price,
-                    exit_reference_price=price,
-                    timestamp=opentime,
-                )
-
-        if side == "Zeroed":
-            order_amount = portfolio.quote
-
-            if order_amount > self.parameters.min_lot_size:
-                fee_quote = fee_rate_decimal * order_amount
-                bought_base_amount = price * (order_amount - fee_quote)
-
-                portfolio.quote -= order_amount
-                portfolio.base += bought_base_amount
-
-                order_dict = dict(
-                    timestamp=opentime,
-                    signal="SELL",
-                    price=price,
-                    fee=price * fee_quote,
-                    quote_amount=order_amount,
-                )
-                self.operation.report_trade(order_dict)
-                self.operation.position.update(
-                    side="Zeroed",
-                    enter_price=price,
-                    exit_reference_price=price,
-                    timestamp=opentime,
-                )
-
-        self.update_result(portfolio, price)
-
-
-class RealTradingOrderHandler(OrderHandler):
-    def __init__(self, operation: Operation):
-        super().__init__(operation)
-
-    def current_price(self):
-        return self.broker.get_price()
-
-    def get_portfolio(self) -> Portfolio:
-        return self.broker.get_portfolio()
-
-    def proceed(self):
-        price = self.current_price()
-        portfolio = self.get_portfolio()
-        side = self.operation.current_result.Side.item()
-        fee_rate_decimal = self.broker.fee_rate_decimal
-        leverage = self.operation.current_result.Leverage.item()
-
-        order = Order(
-            test_order=self.setup.test_order,
-            ticker_symbol=(
-                self.setup.market.quote_symbol + self.setup.market.base_symbol
-            ).upper(),
-            side=str(),
-            order_type=self.setup.order_type,
-            quantity=0.0,
-            price=price,
+    def _buy(self):
+        self.order.signal = sig.buy
+        self.order.quantity = 0.998 * (
+            self.order.leverage
+            * (self.order.portfolio.base / self.order.price)
         )
-        if not self.setup.allow_naked_sells and side == "Short":
-            side = "Zeroed"
+        return self.broker.execute_order(self.order)
 
-        if side == "Long":
-            order_amount = 0.998 * (leverage * (portfolio.base / price))
+    def _sell(self):
+        self.order.signal = sig.sell
+        self.order.quantity = 0.998 * self.order.portfolio.quote
+        return self.broker.execute_order(self.order)
 
-            if order_amount > self.parameters.min_lot_size:
-                fee_quote = fee_rate_decimal * order_amount
-                bought_quote_amount = order_amount - fee_quote
+    def _naked_sell(self):
+        pass
 
-                order.side = "buy"
-                order.quantity = order_amount
-                self.notifier.trade(order.json())
+    def _double_naked_sell(self):
+        pass
 
-                checked_order = self.broker.execute_order(order)
+    def _double_buy(self):
+        pass
 
-                if checked_order:
-                    self.operation.report_trade(checked_order)
-                    self.operation.position.update(
-                        side="Long",
-                        enter_price=price,
-                        exit_reference_price=price,
-                        timestamp=checked_order["timestamp"],
-                    )
-                    portfolio.quote += bought_quote_amount
-                    portfolio.base -= order_amount * price
+    def _long_stopped(self):
+        pass
 
-        if side == "Zeroed":
-            order_amount = 0.998 * (portfolio.quote)
+    def _short_stopped(self):
+        pass
 
-            if order_amount > self.parameters.min_lot_size:
-                fee_quote = fee_rate_decimal * order_amount
-                bought_base_amount = price * (order_amount - fee_quote)
+    def proceed(
+        self,
+        at_time: int,
+        analysis_result: pd.core.frame.DataFrame,
+        by_stop: bool = False,
+    ):
+        generated_signal = Signal(
+            from_side=self.operation.position.side,
+            to_side=analysis_result.Side.item(),
+            by_stop=by_stop,
+        ).generate()
+        self.order.at_time = at_time
+        self.order.leverage = analysis_result.Leverage.item()
+        self.order.price = self.broker.get_price()
+        self.order.portfolio = self.broker.get_portfolio()
 
-                order.side = "sell"
-                order.quantity = order_amount
-                self.notifier.trade(order.json())
-
-                checked_order = self.broker.execute_order(order)
-
-                if checked_order:
-                    self.operation.report_trade(checked_order)
-                    self.operation.position.update(
-                        side="Zeroed",
-                        enter_price=price,
-                        exit_reference_price=price,
-                        timestamp=checked_order["timestamp"],
-                    )
-
-                portfolio.quote -= order_amount
-                portfolio.base += bought_base_amount
-
-        self.update_result(portfolio, price)
+        _order = getattr(self, "_{}".format(generated_signal))
+        return _order  # Na verdade, fará atualizações nos registros...
 
 
-def get_order_handler(
-    operation: Operation,
-) -> Union[BacktestingOrderHandler, RealTradingOrderHandler]:
-
-    backtesting = (
-        Deserialize(name="setup").from_json(operation.setup)
-    ).backtesting
-
-    if backtesting:
-        return BacktestingOrderHandler(operation)
-    return RealTradingOrderHandler(operation)
+def get_order_handler(operation: Operation) -> OrderHandler:
+    return OrderHandler(operation)
