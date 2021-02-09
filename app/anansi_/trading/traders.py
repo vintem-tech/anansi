24066dae6bd236b4722adda6f5db9eb320800668
marcusmello <mandealista@gmail.com>
2021-02-09 15:02:07 -0300

    Draft of new Trader design

--- app/anansi_/trading/traders.py
+++ app/anansi_/trading/traders.py
@@ -1,34 +1,41 @@
+""" The needed classes in order to run operations; In other words, here
+are the objects' representation which, when convenientally combined each
+other, will periodically perform market analyzes (including triggering
+orders and updating records in a database) or simulate trading operations
+against historical data (backtestings).
+"""
+
 import time
-from threading import Thread
 
 import pendulum
 
-from ..notifiers.notifiers import get_notifier
-from ..tools.formatting import text_in_lines_from_dict
-from ..tools.serializers import Deserialize
-from ..tools.time_handlers import ParseDateTime
-from .classifiers import Classifier#, get_classifier
-from .stoploss import get_stop
+from ..config.defaults import Messages
+from ..notifiers.notifiers import Notifier
+from .analyzers import Classifier, StopLoss
 from .models import Operation
-from .order_handler import get_order_handler
 
+messages = Messages()
 
-class DefaultTrader(Thread):
+class Trader:
+    """Capable of periodically performing a classification analysis
+    (buy/sell/stop) on a market. A trader must have at least 4 class'
+    attributes:
+
+        - An operational setup and:
+        - A classifier;
+        - A stoploss manager;
+        - A notifier.
+
+    the last 3 must be instantiated for the given operation.
+    """
+
     def __init__(self, operation: Operation):
         self.operation = operation
-        self.setup = Deserialize(name="setup").from_json(operation.setup)
+        self.setup = operation.operational_setup()
         self.classifier = Classifier(operation)
-        self.stoploss = get_stop(operation)
-
-        # O notifier também poderia ser instanciado passando a operação. Assim,
-        # resultados de logs (debbugs e/ou erros) poder ser apendados como
-        # séries temporais no influxdb
-        self.order_handler = get_order_handler(operation)
-        
-        self.notifier = get_notifier(broadcasters=self.setup.broadcasters)
+        self.stoploss = StopLoss(operation)
+        self.notifier = Notifier(operation)
         self.now: int = None  # Datetime (seconds UTC timestamp)
-        self.step: int = None # Seconds
-        super().__init__()
 
     def _start(self):
         self.now = pendulum.now("UTC").int_timestamp
@@ -36,77 +43,37 @@ class DefaultTrader(Thread):
             self.operation.reset()
             self.now = self.classifier.initial_backtesting_now()
 
-        if self.setup.debbug:
-            setup_str = text_in_lines_from_dict(self.setup._asdict())
-            msg = "Starting Anansi. Your operation id is {}\n\n{}".format(
-                self.operation.id, setup_str
-            )
-            self.notifier.debbug(msg)
+        self.notifier.debbug(messages.initial(self.operation.id, self.setup))
         self.operation.update(is_running=True)
 
-    def _stop_analysis(self):
-        self.step = self.classifier.time_until_next_closed_candle()
-        verify_stoploss = bool(
-            self.operation.position.side != "Zeroed" and self.setup.stop_is_on
-        )
-        if verify_stoploss:
-            result = self.stoploss.result_at(desired_datetime=self.now)
-            self.operation.save_result("stoploss", result)
-            self.order_handler.proceed(
-                at_time=self.now,
-                analysis_result=result,
-                by_stop=True,
-            )
-            self.operation.last_check.update(by_stop_loss_at=self.now)
-            self.step = self.stoploss.time_until_next_closed_candle()
-
-    def _classifier_analysis(self):
-        last_check = self.operation.last_check.by_classifier_at
-        step = self.classifier.time_frame_total_seconds()
-        new_candle = bool(self.now >= last_check + step)
-
-        if new_candle:
-            result = self.classifier.restult_at(desired_datetime=self.now)
-            self.operation.save_result("classifier", result)
-            self.order_handler.proceed(
-                at_time=self.now,
-                analysis_result=result,
-            )
-            self.operation.last_check.update(by_classifier_at=self.now)
-
     def _forward_step(self):
-        if self.setup.debbug:
-            now = ParseDateTime(self.now).from_timestamp_to_human_readable()
-            _step = ParseDateTime(self.step).from_timestamp_to_human_readable()
-            msg = """Time now (UTC) = {}\nSleeping {} s.""".format(now, _step)
-            self.notifier.debbug(msg)
+        step = (
+            self.stoploss.time_until_next_closed_candle()
+            if self.stoploss.is_on()
+            else
+            self.classifier.time_until_next_closed_candle()
+        )
+        self.notifier.debbug(messages.time_monitoring(self.now, step))
 
         if self.setup.backtesting.is_on:
-            self.now += self.step
+            self.now += step
+            if self.now >= self.classifier.final_backtesting_now():
+                self.operation.update(is_running=False)
 
         else:
-            time.sleep(self.step)
+            time.sleep(step)
             self.now = (pendulum.now(tz="UTC")).int_timestamp
-
 
     def run(self):
         self._start()
         while self.operation.is_running:
-            # try:
-            self._stop_analysis()
-            self._classifier_analysis()
-            self._forward_step()
+            try:
+                self.stoploss.analyze(at_time=self.now)
+                self.classifier.analyze(at_time=self.now)
+                self._forward_step()
 
-            # except Exception as e:
-            # self.notifier.error(e)
-            # time.sleep(3600)  # 1 hour cooldown
+            except Exception as e:
+                self.notifier.error(e)
+                time.sleep(600)  # 10 min cooldown
 
-        final_msg = "Op. {} finalized!".format(self.operation.id)
-        self.notifier.debbug(final_msg)
-
-    def run_in_background(self):
-        self.start()
-
-class OffSetRealTimeTresholdTrader:
-    pass
-
+        self.notifier.debbug(messages.final(self.operation.id))
