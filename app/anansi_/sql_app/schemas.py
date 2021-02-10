2e171831bcd1d94986c1c76e2f2487be75f20646
marcusmello <mandealista@gmail.com>
2021-02-10 18:10:17 -0300

    Some formatting and no more defaults here

--- app/anansi_/sql_app/schemas.py
+++ app/anansi_/sql_app/schemas.py
@@ -3,7 +3,7 @@
 # pylint: disable=too-few-public-methods
 # pylint: disable=E1136
 
-"""Shared pydantic parameters classes."""
+"""Shared pydantic model classes."""
 
 from typing import Union, Sequence, Optional, List
 from pydantic import BaseModel, validator
@@ -25,8 +25,6 @@ class Market(BaseModel):
     """ Market attributes """
 
     broker_name: str
-    #quote_symbol: str
-    #base_symbol: str
     ticker_symbol: str
 
 
@@ -70,28 +68,29 @@ class Portfolio(BaseModel):
 class DidiIndexSetup(BaseModel):
     """ Setup of Didi index trend indicator """
 
-    price_metrics: str = "ohlc4"
-    number_samples: Sequence[int] = (3, 20, 80)
+    price_metrics: str
+    number_samples: Sequence[int]
 
     @validator("price_metrics")
     @classmethod
     def prevent_invalid_price_metrics(cls, value: str) -> str:
-        """Given a certain price_metrics value, checks if this value are in
-        'possible_price_metrics'.
+        """Given a certain price_metrics value, checks if this value
+        are in 'possible_price_metrics'.
 
         Args:
             value (str): price_metrics
 
         Raises:
-            ValueError: An error occurs if the passed price_metrics is not in {}
+            ValueError: An error occurs if the passed price_metrics is
+            not in {}
 
         Returns:
             str: price_metrics, if it is valid.
         """.format(
             possible_price_metrics
         )
-        valid_value = bool(value in possible_price_metrics)
-        if not valid_value:
+
+        if not value in possible_price_metrics:
             raise ValueError(
                 "Price metrics should be in {}".format(possible_price_metrics)
             )
@@ -103,28 +102,29 @@ class DidiIndexSetup(BaseModel):
         """Check the values in 'number_of_samples' set.
 
         Args:
-            value (Sequence[int]): 'number_of_samples' - Set with the number
-            of candles that must be computed to calculate each moving average.
+            value (Sequence[int]): 'number_of_samples' - Set with the
+            number of candles that must be computed to calculate each
+            moving average.
 
         Raises:
-            ValueError: Raises an error if any value in the set is negative, or
-            ValueError: values are not on ascending order, or
-            ValueError: 'number_of_samples' set are passed with no exactly 3
-            elements.
+            ValueError: Raises an error if any value in the set is
+            negative, or values are not on ascending order, or set
+            'number_of_samples' was passed with no exactly 3 elements.
 
         Returns:
             Sequence[int]: 'number_of_samples' set.
         """
 
-        non_positive_values = bool(False in [bool(x > 0) for x in value])
-        if non_positive_values:
+        all_positives = all([x > 0 for x in value])
+        ascending_order = bool(value[2] > value[1] > value[0])
+        correct_length = bool(len(value) == 3)
+
+        if not all_positives:
             raise ValueError("Number samples must be positive integers.")
 
-        ascending_order = bool((value[2] > value[1] and value[1] > value[0]))
         if not ascending_order:
             raise ValueError("Should be in ascending order.")
 
-        correct_length = bool(len(value) == 3)
         if not correct_length:
             raise ValueError("Expected 3 integer values, no more, no less.")
         return value
@@ -133,38 +133,24 @@ class DidiIndexSetup(BaseModel):
 class BollingerBandsSetup(BaseModel):
     """Bollinger bands setup schema, with default values."""
 
-    price_metrics: str = "ohlc4"
-    number_samples: int = 20
-    number_STDs: float = 2.0
+    price_metrics: str
+    number_samples: int
+    number_STDs: float
 
 
 class DidiClassifierSetup(BaseModel):
     """'Default classifier schema, with default values."""
 
-    didi_index: DidiIndexSetup = DidiIndexSetup()
-    bollinger_bands: BollingerBandsSetup = BollingerBandsSetup()
-    partial_opened_bands_weight: float = 1.0
-    time_frame: str = "6h"
-
-
-
-class Signals(BaseModel):
-    """Stores the possible trading signals"""
-
-    hold: str = "hold"
-    buy:str = "buy"
-    sell:str = "sell"
-    naked_sell:str = "naked_sell"
-    double_naked_sell:str = "double_naked_sell"
-    double_buy:str = "double_buy"
-    long_stopped:str = "long_stopped"
-    short_stopped:str = "short_stopped"
+    didi_index: DidiIndexSetup
+    bollinger_bands: BollingerBandsSetup
+    partial_opened_bands_weight: float
+    time_frame: str
 
 
 class Order(BaseModel):
     """Order parameters collection"""
 
-    order_id: Optional[Union[str,int]]
+    order_id: Optional[Union[str, int]]
     test_order: Optional[bool] = False
     order_type: Optional[str]
 
@@ -184,45 +170,40 @@ class Order(BaseModel):
     fee: Optional[float]
     warnings: Optional[str]
 
+
 class BackTesting(BaseModel):
+    """Information about backtesting attributes"""
     is_on: bool
-    price_metrics: str = "ohlc4"
-    fee_rate_decimal :float = 0.001
-    initial_portfolio = Portfolio(quote=0.0, base=1000.00)
+    price_metrics: str
+    fee_rate_decimal: float
+    initial_portfolio: Portfolio
+
 
 class Classifier(BaseModel):
+    """Information about classifier attributes"""
     name: str
     setup: BaseModel
 
+
 class StopLoss(BaseModel):
+    """Information about stoploss attributes"""
     is_on: bool
     name: str
     setup: BaseModel
 
-class NotifierLevels: # Muito possivelmente desnecessário, já que serão atributos do notifier.
-    debug: str = "debug"
-    info: str = "info"
-    warning: str = "warning"
-    error: str  = "error"
 
-class NotifierBroadcasters:
-    default_print: str = "default_print"
-    default_log:str = "default_log"
-    telegram:str = "telegram"
-    whatsapp:str = "whatsapp"
-    email:str = "email"
-    push:str = "push"
-
-
-class Notifier:
+class Notifier(BaseModel):
+    """Information about notifier attributes"""
     debug: bool
     broadcasters: List
 
-class Treshold(BaseModel):
-    """Given n measurements, fires the trigger when n positives are achieved"""
 
-    n_measurements: int
-    n_positives: int
+class Treshold(BaseModel):
+    """Fires the trigger when 'm_found' true conditions are found among
+    the 'n_datapoints' values."""
+
+    m_found: int
+    n_datapoints: int
 
 
 class Trigger(BaseModel):
@@ -235,20 +216,13 @@ class Trigger(BaseModel):
 class StopTrailing3T(BaseModel):
     """Triple Trigger Treshold Stop Trailing setup"""
 
-    time_frame = "5m"
-    price_metric = "oc2"
-    first_trigger = Trigger(
-        rate=5, treshold=Treshold(n_measurements=6, n_positives=3)
-    )
-    second_trigger = Trigger(
-        rate=3, treshold=Treshold(n_measurements=18, n_positives=12)
-    )
-    third_trigger = Trigger(
-        rate=1, treshold=Treshold(n_measurements=36, n_positives=20)
-    )
-    update_target_if = Trigger(
-        rate=0.7, treshold=Treshold(n_measurements=10, n_positives=7)
-    )
+    time_frame:str
+    price_metric:str
+    first_trigger: Trigger
+    second_trigger: Trigger
+    third_trigger: Trigger
+    update_target_if: Trigger
+
 
 class OperationalSetup(BaseModel):
     """Operational schema, with default values."""
@@ -261,3 +235,27 @@ class OperationalSetup(BaseModel):
     market: Market
     default_order_type: str
     allow_naked_sells: bool
+
+
+# Parameters collections
+class Signals(BaseModel):
+    """Stores the possible trading signals"""
+
+    hold: str = "hold"
+    buy: str = "buy"
+    sell: str = "sell"
+    naked_sell: str = "naked_sell"
+    double_naked_sell: str = "double_naked_sell"
+    double_buy: str = "double_buy"
+    long_stopped: str = "long_stopped"
+    short_stopped: str = "short_stopped"
+
+
+class NotifierBroadcasters(BaseModel):
+    """Stores the possible trading signals"""
+
+    print_on_screen: str = "print_on_screen"
+    telegram: str = "telegram"
+    whatsapp: str = "whatsapp"
+    email: str = "email"
+    push: str = "push"
