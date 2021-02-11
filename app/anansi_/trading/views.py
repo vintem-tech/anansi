646c16d4cc89986b01f06498f5f06fdc6d586f77
marcusmello <mandealista@gmail.com>
2021-02-11 20:18:36 -0300

    A new way to get the operational setup

--- app/anansi_/trading/views.py
+++ app/anansi_/trading/views.py
@@ -1,11 +1,11 @@
 from pony.orm import db_session
-from . import defaults
-from .models import Position, Operation, TradeLog, LastCheck
+from ..config.defaults import get_operational_setup_for
+from .models import Position, Operation, LastCheck
 
 @db_session
 def create_default_operation(operation_mode: str):
     Operation(
-        setup=getattr(defaults, operation_mode).json(),
+        setup=get_operational_setup_for(operation_mode),
         position=Position(),
         last_check=LastCheck(by_classifier_at=0),
     )
