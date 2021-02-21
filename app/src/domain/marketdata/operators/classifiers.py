f888332edb6f1f69e3ad250c2e7c8d0d00fb2d84
marcusmello <mandealista@gmail.com>
2021-02-21 09:12:08 -0300

    Modifying in order to get a specific result length, and compute delay between slow and fast 'didi ratios' inversions

--- app/src/domain/marketdata/operators/classifiers.py
+++ app/src/domain/marketdata/operators/classifiers.py
@@ -15,40 +15,58 @@ thismodule = sys.modules[__name__]
 
 
 class DidiClassifier:
-    def __init__(self, market: Market, setup: BaseModel, backtesting=False):
+    class DidiInversion:
+        index_of_slow:int = 0
+        index_of_fast:int = 0
+
+        def reset(self):
+            self.index_of_slow = 0
+            self.index_of_fast = 0
+
+    def __init__(
+        self,
+        market: Market,
+        setup: BaseModel,
+        backtesting: bool = False,
+        result_length: int = 1,
+    ):
         self.setup = setup
         self.klines_getter = klines_getter(
             market=market,
             time_frame=setup.time_frame,
             backtesting=backtesting,
         )
-        self.data = pd.DataFrame()
         self.result = pd.DataFrame()
+        self.data = pd.DataFrame()
+        self.didi_inversion = self.DidiInversion()
+        self.result_length = result_length
+        self.extra_length = 10
 
     def number_of_samples(self):
-        return max(
-            max(self.setup.didi_index.number_samples),
-            self.setup.bollinger_bands.number_samples,
-        )
+        return (
+            max(
+                max(self.setup.didi_index.number_samples),
+                self.setup.bollinger_bands.number_samples,
+            )
+        ) + self.result_length + self.extra_length
 
     def get_data_until(self, desired_datetime: DateTimeType) -> None:
         self.data = self.klines_getter.get(
             until=desired_datetime, number_of_candles=self.number_of_samples()
         )
 
-    def _trend_analysis(self):
-        slow = self.data.Didi_slow.tail(1).item()
-        fast = self.data.Didi_fast.tail(1).item()
+    def apply_indicators_pipeline(self):
+        self.data.apply_indicator.trend.didi_index(setup=self.setup.didi_index)
+        self.data.apply_indicator.volatility.bollinger_bands(
+            setup=self.setup.bollinger_bands
+        )
 
-        didi_result = 1 if slow < 0 < fast else -1 if fast < 0 < slow else 0
-        self.result["Trend_result"] = didi_result
+    def _bollinger_analysis(self, result, index:int):
+        previous_bb_upper = result.iloc[index].BB_upper
+        current_bb_upper = result.iloc[index + 1].BB_upper
 
-    def _volatility_analysis(self):
-        previous_bb_upper = self.data[-2:-1].BB_upper.item()
-        current_bb_upper = self.data[-1:].BB_upper.item()
-
-        previous_bb_lower = self.data[-2:-1].BB_lower.item()
-        current_bb_lower = self.data[-1:].BB_lower.item()
+        previous_bb_lower = result.iloc[index].BB_lower
+        current_bb_lower = result.iloc[index + 1].BB_lower
 
         total_opened_bands = bool(
             (current_bb_upper > previous_bb_upper)
@@ -58,48 +76,51 @@ class DidiClassifier:
             (current_bb_upper <= previous_bb_upper)
             and (current_bb_lower >= previous_bb_lower)
         )
-        bollinger_weight = (
+        self.result.loc[index + 1, "Bollinger"] = (
             1.0
             if total_opened_bands
             else 0.0
             if total_closed_bands
             else self.setup.partial_opened_bands_weight
         )
-        self.result["Bollinger_weight"] = bollinger_weight
 
-    def _apply_indicators_pipeline(self):
-        self.data.apply_indicator.trend.didi_index(setup=self.setup.didi_index)
-        self.data.apply_indicator.volatility.bollinger_bands(
-            setup=self.setup.bollinger_bands
-        )
+    def _didi_analysis(self, result, index:int):
+        previous_slow = result.iloc[index].Didi_slow
+        slow = result.iloc[index + 1].Didi_slow
+        slow_inversion = bool(slow/previous_slow < 0)
 
-    def _evaluate_indicators_results(self):
+        previous_fast = result.iloc[index].Didi_fast
+        fast = result.iloc[index + 1].Didi_fast
+        fast_inversion = bool(fast/previous_fast < 0)
+
+        if slow_inversion:
+            self.didi_inversion.index_of_slow = index + 1
+        if fast_inversion:
+            self.didi_inversion.index_of_fast = index + 1
+
+        #Aqui deve fechar a análise de tendência, a partir da idéia de "alerta/confirmação"
+
+        self.result.loc[index + 1, "Didi_index"] = (1 if slow < 0 < fast else -1 if fast < 0 < slow else 0)
+
+    def evaluate_indicators_results(self):
+        self.didi_inversion.reset()
+        _len = self.result_length + self.extra_length
         self.result = pd.DataFrame()
-        self.result = self.result.append(self.data[-1:], ignore_index=True)
+        self.result = self.result.append(self.data[-_len:], ignore_index=True)
 
-        self._trend_analysis()
-        self._volatility_analysis()
+        for i in range(_len - 1):
+            result = self.result[i:i+1]
+            self._bollinger_analysis(result, i)
+            self._didi_analysis(result, i)
 
-        leverage = (
-            self.result.Trend_result.item()
-            * self.result.Bollinger_weight.item()
-        )
-        side = (
-            "Long"
-            if leverage > 0.3
-            else "Short"
-            if leverage < -0.3
-            else "Zeroed"
-        )
-        self.result["Side"] = side
-        self.result["Leverage"] = abs(leverage)
+    def get_restult_at(
+        self, desired_datetime: DateTimeType
+    ) -> pd.core.frame.DataFrame:
 
-    def get_restult_at(self, desired_datetime: int) -> pd.core.frame.DataFrame:
         self.get_data_until(desired_datetime)
-        self._apply_indicators_pipeline()
-        self._evaluate_indicators_results()
-
-        return self.result
+        self.apply_indicators_pipeline()
+        self.evaluate_indicators_results()
+        return self.result[-self.result_length:]
 
 
 def get_classifier(payload: ClassifierPayLoad):
@@ -111,6 +132,9 @@ def get_classifier(payload: ClassifierPayLoad):
     Returns: Union[DidiClassifier]: The classifier
     """
 
-    return getattr(thismodule, payload.classifier.name)(
-        payload.market, payload.setup, payload.backtesting
+    return getattr(thismodule, payload.name)(
+        market=payload.market,
+        setup=payload.setup,
+        backtesting=payload.backtesting,
+        result_length=payload.result_length,
     )
