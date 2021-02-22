30d3632361e0f178f013a768aeaa7d9e0b977c36
marcusmello <mandealista@gmail.com>
2021-02-22 10:44:11 -0300

    y

--- app/src/views/trading.py
+++ app/src/views/trading.py
@@ -1,11 +1,24 @@
+"""Interface between trade logic and the user"""
+
 from pony.orm import db_session
-from ..config.defaults import get_operational_setup_for
-from .models import Position, Operation, LastCheck
+from .defaults import get_operational_setup_for
+from ..repositories.sql_app.models import Position, Operation, LastCheck
+from ..repositories.sql_app.schemas import OperationalSetup
 
 @db_session
-def create_default_operation(operation_mode: str):
+def create_operation(operational_setup:OperationalSetup):
+    """Given an operational setup, creates an operation"""
+
     Operation(
-        json_setup=get_operational_setup_for(operation_mode),
+        json_setup=operational_setup,
         position=Position(),
         last_check=LastCheck(by_classifier_at=0),
     )
+
+@db_session
+def create_default_operation(operation_mode: str):
+    """Given an operation mode ('backtesting', 'real_trading' or
+    'test_trading'), creates an operation"""
+
+    setup = get_operational_setup_for(operation_mode)
+    create_operation(setup)
