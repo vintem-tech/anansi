3be2ead503acf2f1b1c48877f5bd5677a0686277
marcusmello <mandealista@gmail.com>
2021-02-23 20:56:49 -0300

    It seems a good ideia split operations into monitors

--- app/src/repositories/sql_app/models.py
+++ app/src/repositories/sql_app/models.py
@@ -5,12 +5,12 @@ from pony.orm import Database, Json, Optional, Required, Set, commit, sql_debug
 
 from ...config.settings import system_settings
 from ...tools.serializers import Deserialize
-from ..sql_app.schemas import OperationalSetup
+from ..sql_app.schemas import Modes
 from ..time_series_storage import StorageResults
 
 settings = system_settings()
 db = Database()
-
+modes = Modes()
 db.bind(**settings.relational_database.dict())
 sql_debug(settings.sql_debug)
 
@@ -29,7 +29,7 @@ class Portfolio(db.Entity, AttributeUpdater):
 
 
 class Position(db.Entity, AttributeUpdater):
-    operation = Optional(lambda: Operation)  # Foreing key
+    monitor = Optional(lambda: Monitor)  # Foreing key
     portfolio = Optional(Portfolio, cascade_delete=True)
     side = Required(str, default="Zeroed")
     enter_price = Optional(float)
@@ -38,17 +38,18 @@ class Position(db.Entity, AttributeUpdater):
 
 
 class LastCheck(db.Entity, AttributeUpdater):
-    Operation = Optional(lambda: Operation)  # Foreing key
+    Monitor = Optional(lambda: Monitor)  # Foreing key
     by_classifier_at = Optional(int)  # UTC timestamp
     by_stop_loss_at = Optional(int)  # UTC timestamp
 
 
-class Operation(db.Entity, AttributeUpdater):
-    json_setup = Optional(Json)
+class Monitor(db.Entity, AttributeUpdater):
+    operation = Optional(lambda: Operation)
+    _setup = Optional(Json)
     position = Required(Position, cascade_delete=True)
     last_check = Required(LastCheck, cascade_delete=True)
     trade_log = Set(lambda: TradeLog, cascade_delete=True)
-    is_running = Required(bool, default=True)
+    #is_running = Required(bool, default=True)
 
     def save_result(
         self, database: str, result: pd.core.frame.DataFrame
@@ -63,24 +64,40 @@ class Operation(db.Entity, AttributeUpdater):
         self.trade_log.create(**payload)
         commit()
 
+    def setup(self):
+        return Deserialize(name="setup").from_json(self._setup)
+
     def reset(self):
         self.last_check.update(by_classifier_at=0)
         self.position.update(side="Zeroed")
-        self.position.portfolio.update(quote=0.0, base=1000.00)
+        self.position.portfolio.update(quote=0.0, base=0.0)
         self.trade_log.clear()
         commit()
 
-    def operational_setup(self) -> OperationalSetup:
-        return Deserialize(name="setup").from_json(self.json_setup)
-
 
 class TradeLog(db.Entity):
-    operation = Optional(lambda: Operation)
+    monitor = Optional(lambda: Monitor)
     signal = Optional(str)
     timestamp = Optional(int)
     price = Optional(float)
     fee = Optional(float)  # base_asset units
     quote_amount = Optional(float)
+
+class Operation(db.Entity):
+    monitors = Set(Monitor, cascade_delete=True)
+    mode = Required(str, default=modes.backtesting)
+    is_running = Required(bool, default=False)
+    _notifier_setup = Required(Json)
+    _backtesting_setup = Optional(Json)
+
+    def notifier_setup(self):
+        return Deserialize(name="setup").from_json(self._notifier_setup)
+
+    def backtesting_setup(self):
+        return Deserialize(name="setup").from_json(self._backtesting_setup)
+    
+    def reset(self):
+        raise NotImplementedError
 
 
 db.generate_mapping(create_tables=True)
