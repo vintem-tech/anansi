74e47dc60b7da74fc7b98101926f4ad8799f5c03
marcusmello <mandealista@gmail.com>
2021-02-23 20:56:55 -0300

    First steps in order to fit to the 'operation/monitors' logic

--- app/src/views/trading.py
+++ app/src/views/trading.py
@@ -1,9 +1,24 @@
 """Interface between trade logic and the user"""
 
 from pony.orm import db_session
-from .defaults import get_operational_setup_for
-from ..repositories.sql_app.models import Position, Operation, LastCheck
-from ..repositories.sql_app.schemas import OperationalSetup
+from .defaults import get_operational_setup_for, monitor_setup
+from ..repositories.sql_app.models import Position, Monitor, Operation, LastCheck
+from ..repositories.sql_app.schemas import MonitorSetup
+
+@db_session
+def create_monitor(setup: MonitorSetup):
+    Monitor(
+        _setup=setup,
+        position=Position(),
+        last_check=LastCheck(by_classifier_at=0),
+    )
+
+@db_session
+def create_default_monitor():
+    create_monitor(setup=monitor_setup)
+
+
+# Estudar na documentação do Pony como criar objetos relacionados
 
 @db_session
 def create_operation(operational_setup:OperationalSetup):
