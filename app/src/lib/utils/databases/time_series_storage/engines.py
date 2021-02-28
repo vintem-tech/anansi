0ec4ee37e5a5df9a78c87fa633a8c7252ee1994c
marcusmello <mandealista@gmail.com>
2021-02-28 13:10:39 -0300

    Updating influxdb to version 2; append is working for now

--- app/src/lib/utils/databases/time_series_storage/engines.py
+++ app/src/lib/utils/databases/time_series_storage/engines.py
@@ -6,32 +6,32 @@ designed for saving klines, it must be able to read write timeseries
 collections from/to files and/or databases, abstracting the needed
 implementations."""
 
-import collections
+#import collections
 
 import sys
 import pandas as pd
-from influxdb import DataFrameClient  # Influxdb v1.8
-from .....config.settings import TimeSerieDatabaseEngines
 
-engines = TimeSerieDatabaseEngines()
+# from influxdb import DataFrameClient  # Influxdb v1.8
+from influxdb_client import InfluxDBClient, WriteOptions
+from .....config.settings import InfluxDbSettings
+
+influx_settings = InfluxDbSettings()
 
 thismodule = sys.modules[__name__]
 
 
 class InfluxDb:
     """For influxdb information:
-    https://influxdb-python.readthedocs.io/en/latest/api-documentation.html#dataframeclient
+    https://github.com/influxdata/influxdb-client-python
     """
 
     __slots__ = [
-        "client",
-        "database",
+        "bucket",
         "measurement",
     ]
 
-    def __init__(self, database: str, measurement: str):
-        self.client = DataFrameClient(**engines.influx_db)
-        self.database = database
+    def __init__(self, bucket: str, measurement: str):
+        self.bucket = bucket
         self.measurement = measurement
 
     def append(self, dataframe: pd.core.frame.DataFrame) -> None:
@@ -39,84 +39,25 @@ class InfluxDb:
 
         Args:
             dataframe (pd.core.frame.DataFrame): Timeseries dataframe
-
-        For retention policy, follow below example, and declare the
-        'retention_policy=name' at 'client.write_points':
-
-        client.create_retention_policy(name="long_duration",
-        replication=1, duration='INF') or
-
-        client.alter_retention_policy(name="short_duration",
-        replication=1, duration='10d')
         """
-        self.client.create_database(self.database)
 
-        self.client.write_points(
-            dataframe=dataframe,
-            measurement=self.measurement,
-            tags=None,
-            tag_columns=None,
-            field_columns=list(dataframe.columns),
-            time_precision=None,
-            database=self.database,
-            # retention_policy=None,
-            batch_size=None,
-            protocol="line",
-            numeric_precision=None,
+        client = InfluxDBClient(**influx_settings.credentials)
+        write_client = client.write_api(
+            write_options=WriteOptions(**influx_settings.write_opts)
         )
-        self.client.close()
+        write_client.write(
+            bucket=self.bucket,
+            org=client.org,
+            record=dataframe,
+            data_frame_measurement_name=self.measurement,
+        )
+        client.close()
 
-    def proceed(self, query_to_proceed) -> collections.defaultdict:
-        query_result = self.client.query(query_to_proceed)
-        self.client.close()
-        return query_result
+    #    def proceed(self, query_to_proceed) -> collections.defaultdict:
+    #        query_result = self.client.query(query_to_proceed)
+    #        self.client.close()
+    #        return query_result
 
 
 def instantiate_engine(engine_name: str):
     return getattr(thismodule, engine_name)
-
-
-# (influxdb:v2.0.2)
-
-# ## Image for docker-compose:
-
-#   influxdb:
-#     image: quay.io/influxdb/influxdb:v2.0.2
-
-
-# ## References:
-
-# https://github.com/influxdata/influxdb-client-python
-# https://docs.influxdata.com/influxdb/v2.0/tools/client-libraries/python/
-
-# ## Basic script to append dataframes to buckets
-
-# import influxdb_client
-# from influxdb_client.client.write_api import SYNCHRONOUS
-# import pandas as pd
-
-# bucket = "<bucket>"
-# org = "<organization>"
-# token = "<token>"
-# url="http://localhost:8086"
-
-# client = influxdb_client.InfluxDBClient(
-#   url=url,
-#   token=token,
-#   org=org,
-#   enable_gzip=True
-# )
-
-# write_api = client.write_api(write_options=SYNCHRONOUS)
-
-# work_klines = my_klines.copy()
-
-# work_klines.KlinesDateTime.from_human_readable_to_timestamp()
-
-# work_klines.Open_time = pd.to_datetime(work_klines.Open_time, unit="s")
-# work_klines.set_index("<date_time-timestamp>", inplace=True)
-
-# write_api .write(bucket=bucket,
-#                  org=org,
-#                  record=work_klines,
-#                  data_frame_measurement_name='<measurement_name>')
