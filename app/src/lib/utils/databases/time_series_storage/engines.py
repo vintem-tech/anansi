7a75db117207ac486a4d2bdc03e1a3a93d1dfa0f
marcusmello <mandealista@gmail.com>
2021-03-14 13:23:42 -0300

    Influxdb versions 1 and 2 at the same module

--- app/src/lib/utils/databases/time_series_storage/engines.py
+++ app/src/lib/utils/databases/time_series_storage/engines.py
@@ -1,37 +1,68 @@
-import collections
+# pylint: disable=unexpected-keyword-arg
+# pylint: disable=no-value-for-parameter
 
+"""An interface layer over influxdb timeseries database.
+Documentation: https://www.influxdata.com/"""
+
+import collections
 import sys
+
 import pandas as pd
-from environs import Env
-from influxdb import DataFrameClient  # Influxdb v1.8
+from influxdb import DataFrameClient  # For influxdb v1.x
+
+# For influxdb v2.x:
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.exceptions import InfluxDBError
+from influxdb_client.client.write_api import ASYNCHRONOUS
+
+from .....config.settings import InfluxDbSettings
 
 thismodule = sys.modules[__name__]
 
-env = Env()
+class Engine:
+    __slots__ = [
+        "database",
+        "table",
+    ]
 
-influxdb_params = dict(
-    host=env.str("INFLUXDB_HOST", default="localhost"),
-    port=env.int("INFLUXDB_PORT", default=8086),
-    username=env.str("INFLUXDB_USER", default="Anansi"),
-    password=env.str("INFLUXDB_USER_PASSWORD", default="anansi2020"),
-    gzip=env.bool("INFLUXDB_GZIP", default=True),
-)
+    def __init__(self, database: str, table: str):
+        self.database = database
+        self.table = table
 
+    def append(self, dataframe: pd.core.frame.DataFrame) -> None:
+        raise NotImplementedError
 
-class InfluxDb:
+    def dataframe_query(self, query: str) -> pd.core.frame.DataFrame:
+        raise NotImplementedError
 
-    """For influxdb information:
-    https://influxdb-python.readthedocs.io/en/latest/api-documentation.html#dataframeclient
+    def get(self, **kwargs) -> pd.core.frame.DataFrame:
+        """Solves the request, if that contains at least 2 of these
+        3 arguments: "start", "stop", "n".
+
+        Returns:
+            pd.core.frame.DataFrame: Requested measurement range.
+        """
+
+        raise NotImplementedError
+
+    def oldest(self, n=1):
+        raise NotImplementedError
+
+    def newest(self, n=1):
+        raise NotImplementedError
+
+class InfluxDbV1(Engine):
+    """Documentation:
+    https://influxdb-python.readthedocs.io/en/latest/api-documentation.html
     """
 
     __slots__ = [
-        "database",
-        "measurement",
+        "settings",
     ]
 
-    def __init__(self, database: str, measurement: str):
-        self.database = database
-        self.measurement = measurement
+    def __init__(self, database: str, table: str):
+        super().__init__(database, table)
+        self.settings = InfluxDbSettings().V1
 
     def append(self, dataframe: pd.core.frame.DataFrame) -> None:
         """Saves a timeseries pandas dataframe on influxdb.
@@ -48,26 +79,199 @@ class InfluxDb:
         client.alter_retention_policy(name="short_duration",
         replication=1, duration='10d')
         """
-        client = DataFrameClient(**influxdb_params)
+        client = DataFrameClient(**self.settings.credentials)
         client.create_database(self.database)
 
         client.write_points(
             dataframe=dataframe,
-            measurement=self.measurement,
+            measurement=self.table,
             tags=None,
             tag_columns=None,
             field_columns=list(dataframe.columns),
             time_precision=None,
             database=self.database,
-            # retention_policy=None,
             batch_size=None,
             protocol="line",
             numeric_precision=None,
         )
         client.close()
 
-    def proceed(self, query_to_proceed) -> collections.defaultdict:
-        client = DataFrameClient(**influxdb_params)
-        query_result = client.query(query_to_proceed)
+    def dataframe_query(self, query) -> collections.defaultdict:
+        """Given a sanitized query (InfluxQL language), returns the
+        requested measurement formated as a pandas dataframe."""
+        client = DataFrameClient(**self.settings.credentials)
+        query_result = client.query(query)
         client.close()
         return query_result
+
+    def get(self, **kwargs) -> pd.core.frame.DataFrame:
+        raise NotImplementedError
+
+    def oldest(self, n=1):
+        raise NotImplementedError
+
+    def newest(self, n=1):
+        raise NotImplementedError
+
+
+class InfluxDbV2(Engine):
+    """An interface layer on top of the influxdb v2.x python client,
+    useful for the rest of the system.
+
+    Documentation:
+    https://github.com/influxdata/influxdb-client-python
+    """
+
+    __slots__ = [
+        "settings",
+        "drop_influxdb_columns",
+    ]
+
+    def __init__(self, database: str, table: str):
+        super().__init__(database, table)
+        self.settings = InfluxDbSettings().V2
+        self.drop_influxdb_columns = True
+
+    def append(self, dataframe: pd.core.frame.DataFrame) -> None:
+        """Saves a timeseries pandas dataframe on influxdb.
+
+        Args:
+            dataframe (pd.core.frame.DataFrame): Timeseries dataframe
+        """
+
+        dataframe.Timestamp = pd.to_datetime(dataframe.Timestamp, unit="s")
+        dataframe.set_index("Timestamp", inplace=True)
+
+        client = InfluxDBClient(**self.settings.credentials)
+        write_client = client.write_api(
+            # write_options=WriteOptions(**self.settings.write_opts),
+            write_options=ASYNCHRONOUS,
+        )
+        write_client.write(
+            bucket=self.database,
+            org=client.org,
+            record=dataframe,
+            data_frame_measurement_name=self.table,
+        )
+        client.close()
+
+    def _drop_influxdb_columns(
+        self,
+        dataframe: pd.core.frame.DataFrame,
+    ) -> pd.core.frame.DataFrame:
+        returned_columns = list(dataframe.columns)
+        columns_to_drop = [
+            column
+            for column in returned_columns
+            if column in self.settings.system_columns
+        ]
+        return dataframe.drop(columns=columns_to_drop)
+
+    def dataframe_query(self, query: str) -> pd.core.frame.DataFrame:
+        """Given a sanitized query*, (flux language format, influxdb v2),
+        returns the request measurement formated as a pandas dataframe.
+        Args:
+            query (str): flux query*
+            drop_influxdb_columns (bool, optional): If True, drops the
+            columns created by the influxdb. Defaults to True.
+
+        Returns:
+            pd.core.frame.DataFrame: Pandas dataframe, created from the
+            returned influxdb measurement, following the conversion:
+
+            [influx]                 -> [dataframe]
+            table.fields.values      -> columns
+            table.records.row.values -> row
+        """
+
+        client = InfluxDBClient(**self.settings.credentials)
+        query_api = client.query_api()
+
+        try:
+            dataframe = query_api.query_data_frame(query)
+            if not dataframe.empty:
+                dataframe = dataframe.rename(columns={"_time": "Timestamp"})
+                dataframe.Timestamp = (
+                    dataframe.Timestamp.astype("int64") // 10 ** 9
+                )
+                if self.drop_influxdb_columns:
+                    dataframe = self._drop_influxdb_columns(dataframe)
+
+            client.close()
+            return dataframe
+        except InfluxDBError as error:
+            raise Exception.with_traceback(error) from InfluxDBError
+        return None
+
+    def _get_by_range(self, start: int, stop: int) -> pd.core.frame.DataFrame:
+        query = """
+            from(bucket: "{}")
+            |> range(start: {}, stop: {})
+            |> filter(fn: (r) => r["_measurement"] == "{}")
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+            """.format(
+            self.database, start, stop, self.table
+        )
+        return self.dataframe_query(query)
+
+    def _get_by_start_n(self, start: int, n: int) -> pd.core.frame.DataFrame:
+        query = """
+            from(bucket: "{}")
+            |> range(start: {}, stop: now())
+            |> limit(n:{})
+            |> filter(fn: (r) => r["_measurement"] == "{}")
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+            """.format(
+            self.database, start, n, self.table
+        )
+        return self.dataframe_query(query)
+
+    def _get_by_stop_n(self, stop: int, n: int) -> pd.core.frame.DataFrame:
+        query = """
+            from(bucket: "{}")
+            |> range(start: 0, stop: {})
+            |> tail(n:{})
+            |> filter(fn: (r) => r["_measurement"] == "{}")
+            |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+            """.format(
+            self.database, stop, n, self.table
+        )
+        return self.dataframe_query(query)
+
+    def get(self, **kwargs) -> pd.core.frame.DataFrame:
+        """Solves the request, if that contains at least 2 of these
+        3 arguments: "start", "stop", "n".
+
+        Returns:
+            pd.core.frame.DataFrame: Requested measurement range.
+        """
+
+        start = kwargs.get("start")
+        stop = kwargs.get("stop")
+        n = kwargs.get("n")
+
+        if start and n:  # This cover the scenario "start and stop and n"
+            dataframe = self._get_by_start_n(start, n)
+
+        elif start and stop and not n:
+            dataframe = self._get_by_range(start, stop)
+
+        elif stop and n and not start:
+            dataframe = self._get_by_stop_n(stop, n)
+
+        else:
+            dataframe = pd.DataFrame()
+            raise ValueError(
+                "At least 2 of 3 arguments (start, stop, n) must be passed"
+            )
+        return dataframe
+
+    def oldest(self, n=1):
+        _dataframe = self.get(start="0", n=n)
+        _len = min(n, len(_dataframe))
+        return _dataframe[:_len]
+
+    def newest(self, n=1):
+        _dataframe = self.get(stop="now()", n=n)
+        _len = min(n, len(_dataframe))
+        return _dataframe[-_len:]
