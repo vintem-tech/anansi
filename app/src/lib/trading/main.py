e2ff3ae741facb5a9ef925edfb44ffa6cbb0fe1c
marcusmello <mandealista@gmail.com>
2021-03-17 18:37:44 -0300

    First draft

--- app/src/lib/trading/main.py
+++ app/src/lib/trading/main.py
@@ -1,70 +1,44 @@
-""" Deve lidar com o monitoramento periódicos de mercados, performando as operações """
+from ...log import Notifier
+from ..utils.databases.sql.models import Monitor, Operation
+from ..utils.databases.sql.schemas import DateTimeType, Signals
 
-from ...config.messages import TradingMonitor
-from ...exceptions import TraderError
-from ...repositories.sql_app.models import Monitor
-from ...repositories.sql_app.schemas import Signals as sig
-from ..notifier import OperationalNotifier
-from .analyzers.classifiers import Classifier
-from .analyzers.stoploss import StopLoss
-from .order_handler import OrderHandler
+sig = Signals()
 
-messages = TradingMonitor()
 
-class Signal:
-    def __init__(self, from_side: str, to_side: str, by_stop: bool):
-        self.from_side = from_side.capitalize()
-        self.to_side = to_side.capitalize()
-        self.by_stop = by_stop
+class Analyzer:
+    def __init__(self, monitor: Monitor):
+        self.monitor = monitor
 
-    def generate(self):
-        if self.from_side == self.to_side:
-            return sig.hold
 
-        if self.from_side == "Zeroed":
-            if self.to_side == "Long":
-                return sig.buy
-            if self.to_side == "Short":
-                return sig.naked_sell
+class StopLoss:
+    def __init__(self, monitor: Monitor):
+        self.monitor = monitor
 
-        if self.from_side == "Long":
-            if self.to_side == "Zeroed":
-                if self.by_stop:
-                    return sig.long_stopped
-                return sig.sell
-            if self.to_side == "Short":
-                return sig.double_naked_sell
 
-        if self.from_side == "Short":
-            if self.to_side == "Zeroed":
-                if self.by_stop:
-                    return sig.short_stopped
-                return sig.buy
-            if self.to_side == "Long":
-                return sig.double_buy
-
-class Monitor:
+class OrderHandler:
     def __init__(self, operation: Operation):
         self.operation = operation
-        self.setup = operation.operational_setup()
-        self.classifier = Classifier(operation)
-        self.stoploss = StopLoss(operation)
-        self.notifier = OperationalNotifier(operation)
-        self.order_handler = OrderHandler(operation)
 
 
 class Trader:
-    def __init__(self, monitors:list, backtesting=True):
-        self.monitors = [(operation) for operation in operations]
-        self.backtesting = backtesting
+    def __init__(self, operation: Operation):
+        self.notifier = Notifier(operation)
+        self.order = OrderHandler(operation)
+        self.monitors = operation.list_of_active_monitors()
+        self.analyzers = [Analyzer(monitor) for monitor in self.monitors]
+        self.stop_loss = [StopLoss(monitor) for monitor in self.monitors]
 
-    def start(self):
-        raise NotImplementedError
-    
-    def stop(self):
-        raise NotImplementedError
-    
-    def run(self):
-        self.start()
-        while True:
-            pass
+    def check_at(self, desired_datetime: DateTimeType):
+        for stop in self.stop_loss:
+            stop.check_at(desired_datetime)
+
+        for analyzer in self.analyzers:
+            analyzer.check_at(desired_datetime)
+
+        self.order.handler(
+            analyzers=[
+                analyzer
+                for analyzer in self.analyzers
+                if analyzer.signal != sig.Zeroed
+            ]
+        )
