ea9bd70a346e7922520ccf9f96e8f0e0882b43b8
marcusmello <mandealista@gmail.com>
2021-04-10 13:48:44 -0300

    'Backtesting' and 'Notifier' incorporated to 'OperationalSetup'

--- app/src/API/views/trading.py
+++ app/src/API/views/trading.py
@@ -1,37 +1,30 @@
 # pylint: disable=no-member
 """Interface between trade logic and the user"""
 
+from typing import List
+
 from pony.orm import commit
 
-from ...config.setups import (
-    BackTesting,
-    Notifier,
-    OperationalSetup,
-    BinanceMonitoring,
-)
+from ...config.setups import BinanceMonitoring, OperationalSetup
 from ...lib.utils.databases.sql.models import Operation
-from ...lib.utils.databases.sql.schemas import OperationalModes
+from ...lib.utils.databases.sql.schemas import Market, OperationalModes
 
 modes = OperationalModes()
 
 
 def create_operation_with_monitors(
-    operation_name: str, mode, setup, notifier, market_list, **kwargs
+    operation_name: str,
+    mode: str,
+    setup: OperationalSetup,
+    market_list: List[Market],
+    **kwargs
 ) -> Operation:
 
     payload = dict(
         name=operation_name,
         mode=mode,
         _setup=setup.json(),
-        _notifier=notifier.json(),
     )
-
-    if mode == modes.backtesting:
-        backtesting = kwargs.get("backtesting")
-        if not backtesting:
-            raise KeyError("Backtesting must be passed on backtesting mode")
-
-        payload = dict(**payload, _backtesting=backtesting.json())
 
     operation = Operation(**payload)
     commit()
@@ -45,7 +38,5 @@ def create_default_operation_with_monitors(name: str) -> Operation:
         operation_name=name,
         mode=modes.backtesting,
         setup=OperationalSetup(),
-        notifier=Notifier(),
         market_list=BinanceMonitoring().markets(),
-        backtesting=BackTesting(),
     )
