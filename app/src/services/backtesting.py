f3ade21a60a38f5d0a4c98fd14f0e2c752c8ccf4
marcusmello <mandealista@gmail.com>
2021-04-17 16:11:15 -0300

    Useless draft

--- app/src/services/backtesting.py
+++ app/src/services/backtesting.py
@@ -4,3 +4,6 @@ from ..lib.utils.schemas import Ticker, TimeRange
 def create_or_complete(ticker:Ticker, time_range:TimeRange):
     storage = ToStorage(ticker)
     storage.create_backtesting(since= time_range.since, until= time_range.until)
+
+class BackTesting:
+    pass
