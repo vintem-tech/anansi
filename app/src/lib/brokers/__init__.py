d757d5ed69147b56a044fabdd2c834e44a957f3b
marcusmello <mandealista@gmail.com>
2021-04-23 15:58:53 -0300

    Now, is capable of return real or backtesting brokers

--- app/src/lib/brokers/__init__.py
+++ app/src/lib/brokers/__init__.py
@@ -2,19 +2,26 @@
 
 import sys
 
-from . import base, binance
+from . import base, binance, backtesting
 
 thismodule = sys.modules[__name__]
 
-def get_broker(broker_name:str) -> base.Broker:
+
+def get_broker(broker_name: str) -> base.Broker:
     """Given a market ticker, returns an instantiated broker"""
 
     try:
         module = getattr(thismodule, broker_name.lower())
         class_ = broker_name.capitalize()
 
-        return getattr(module, class_ )()
+        return getattr(module, class_)()
 
     except AttributeError as err:
         err_msg = "Not implented '{}' broker.".format(broker_name)
         raise NotImplementedError(err_msg) from err
+
+
+def get_backtesting_broker(operation):
+    return getattr(
+        backtesting, "{}Backtesting".format(operation.broker.capitalize())
+    )(operation)
