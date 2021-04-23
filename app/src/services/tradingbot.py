0bbecbd4c119dd6a59eeabeb384f4b8540df1160
marcusmello <mandealista@gmail.com>
2021-04-23 15:59:00 -0300

    BinanceMonitoring 'ticker factory' and a new 'create_operation' method

--- app/src/services/tradingbot.py
+++ app/src/services/tradingbot.py
@@ -1,12 +1,58 @@
+# pylint: disable=too-few-public-methods
+
 from typing import List, Union
 
 from ..lib.utils.databases.sql.models import BackTestingOperation
 from ..lib.trading.main import Trader
+from ..lib.utils.schemas import Ticker
+from ..config.setups import OperationalSetup
+
+
+class BinanceMonitoring:
+    """Binance monitored markets"""
+
+    bases: list = [
+        "BTC",
+        "ETH",
+        "NEO",
+        #        "LINK",
+        #        "EOS",
+        #        "LTC",
+        #        "XRP",
+        #        "BNB",
+        #        "ADA",
+        #        "TRX",
+    ]
+    quotes: list = ["USDT", "BTC"]
+
+    def tickers(self) -> List[Ticker]:
+        """Returns a list of binance market tickers to monitoring"""
+
+        return [
+            Ticker(
+                symbol="{}{}".format(base, quote),
+                base=base,
+                quote=quote,
+
+            )
+            for quote in self.quotes
+            for base in self.bases
+            if quote != base
+        ]
 
 
 class BackTesting:
     operations = dict()
     traders = list()
+
+    def create_operation(
+        self,
+        name: str,
+        broker="binance",
+        tickers_list=BinanceMonitoring().tickers(),
+        setup=OperationalSetup().json(),
+    ) -> BackTestingOperation:
+        return BackTestingOperation.create(name, broker, tickers_list, setup)
 
     def get_operations(self) -> List[str]:
         operations_ = BackTestingOperation.select()
