e165059682e2d39df2512badd583050b8ce9f091
marcusmello <mandealista@gmail.com>
2021-04-24 15:16:37 -0300

    Architecture refactoring baregrond

--- app/src/services/backtesting.py
+++
@@ -1,9 +0,0 @@
-from ..lib.marketdata.klines import ToStorage
-from ..lib.utils.schemas import Ticker, TimeRange
-
-def create_or_complete(ticker:Ticker, time_range:TimeRange):
-    storage = ToStorage(ticker)
-    storage.create_backtesting(since= time_range.since, until= time_range.until)
-
-class BackTesting:
-    pass
