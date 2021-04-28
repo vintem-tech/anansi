58cad5a41ad55e95992dfaa95da228382a0fd4d0
marcusmello <mandealista@gmail.com>
2021-04-28 15:32:25 -0300

    'GetInputSanitizer' done and tested!

--- app/src/lib/marketdata/klines/base.py
+++ app/src/lib/marketdata/klines/base.py
@@ -1,63 +1,50 @@
 # pylint: disable=no-name-in-module
 
+import pandas as pd
 from pydantic import BaseModel
 
-from ...utils.schemas import Ticker #, TimeRange
-from ...utils.tools.time_handlers import (datetime_as_integer_timestamp,
-                                          time_frame_to_seconds)
+from ...utils.schemas import Ticker  # , TimeRange
+from ...utils.tools.time_handlers import int_timestamp, time_frame_to_seconds
 
 
 class GetterSettings(BaseModel):
     return_as_human_readable = True
     ignore_unclosed_kline = True
 
-class SanitizeGetInput:
-    pass
 
-
-class Getter:
-
-    def __init__(self, broker_name:str, ticker: Ticker, time_frame: str = str()):
-        self.broker_name: broker_name
-        self.ticker = ticker
-        self.time_frame = time_frame
-        self.getter_settings = GetterSettings()
-        self._oldest_open_time = self.oldest_open_time()
-        self._newest_open_time = self.newest_open_time()
+class GetInputSanitizer(BaseModel):
+    sec_time_frame: int
+    min_timestamp: int
+    max_timestamp: int
 
     def _timestamp_delta(self, number_samples: int) -> int:
-        return int(
-            time_frame_to_seconds(self.time_frame) * (number_samples - 1)
-        )
+        return int(self.sec_time_frame * (number_samples - 1))
 
     def _until(self, since: int, number_samples: int) -> int:
         until = since + self._timestamp_delta(number_samples)
-        return min(until, self._newest_open_time)
+        return min(until, self.max_timestamp)
 
     def _since(self, until: int, number_samples: int) -> int:
         since = until - self._timestamp_delta(number_samples)
-        return max(since, self.oldest_open_time)
+        return max(since, self.min_timestamp)
 
-    def _sanitize_get_input(self, **kwargs) -> tuple:
+    def sanitize(self, **kwargs) -> tuple:
         since = kwargs.get("since")
         until = kwargs.get("until")
         number_samples: int = kwargs.get("number_samples")
 
         if since:
-            since_ = datetime_as_integer_timestamp(since)
-            since = max(since_, self._oldest_open_time)
-
+            since = max(int_timestamp(since), self.min_timestamp)
             if not until and not number_samples:
-                return since, self._newest_open_time
+                return since, self.max_timestamp
 
         if until:
-            until_ = datetime_as_integer_timestamp(until)
-            until = min(until_, self._newest_open_time)
-
+            until = min(int_timestamp(until), self.max_timestamp)
             if not since and not number_samples:
-                return self._oldest_open_time, until
+                return self.min_timestamp, until
 
-        if since and until:  # This cover the scenario "start and stop and n"
+        # Also the scenario "since and until and number_samples"
+        if since and until:
             if since > until:
                 return until, since
             return since, until
@@ -68,8 +55,22 @@ class Getter:
         if number_samples and until and not since:
             return self._since(until, number_samples), until
 
-        return self._oldest_open_time, self._newest_open_time
+        return self.min_timestamp, self.max_timestamp
 
+
+class Getter:
+    def __init__(
+        self, broker_name: str, ticker: Ticker, time_frame: str = str()
+    ):
+        self.broker_name: broker_name
+        self.ticker = ticker
+        self.time_frame = time_frame
+        self.getter_settings = GetterSettings()
+        self.get_input_sanitizer = GetInputSanitizer(
+            sec_time_frame=time_frame_to_seconds(time_frame),
+            min_timestamp=self.oldest_open_time(),
+            max_timestamp=self.newest_open_time(),
+        )
 
     def oldest_open_time(self) -> int:
         """The oldest avaliable open time (integer timestamp)"""
@@ -78,7 +79,6 @@ class Getter:
     def newest_open_time(self) -> int:
         """The newest avaliable open time (integer timestamp)"""
         raise NotImplementedError
-
 
 
 @pd.api.extensions.register_dataframe_accessor("apply_indicator")
