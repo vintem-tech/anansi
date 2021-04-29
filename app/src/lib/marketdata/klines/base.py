838523d5e2f73ee3941565755c50cfa1a2466b65
marcusmello <mandealista@gmail.com>
2021-04-29 16:14:40 -0300

    Some formatting and 'getters' methods

--- app/src/lib/marketdata/klines/base.py
+++ app/src/lib/marketdata/klines/base.py
@@ -3,8 +3,14 @@
 import pandas as pd
 from pydantic import BaseModel
 
-from ...utils.schemas import Ticker  # , TimeRange
-from ...utils.tools.time_handlers import int_timestamp, time_frame_to_seconds
+from ....lib.utils.exceptions import TimeFormatError
+from ....lib.utils.schemas import Ticker
+from ....lib.utils.tools.time_handlers import (
+    int_timestamp,
+    time_frame_to_seconds,
+)
+
+DF = pd.core.frame.DataFrame
 
 
 class GetterSettings(BaseModel):
@@ -55,6 +61,7 @@ class GetInputSanitizer(BaseModel):
         if number_samples and until and not since:
             return self._since(until, number_samples), until
 
+        # No inputs scenario
         return self.min_timestamp, self.max_timestamp
 
 
@@ -65,8 +72,8 @@ class Getter:
         self.broker_name: broker_name
         self.ticker = ticker
         self.time_frame = time_frame
-        self.getter_settings = GetterSettings()
-        self.get_input_sanitizer = GetInputSanitizer(
+        self.settings = GetterSettings()
+        self.sanitizer = GetInputSanitizer(
             sec_time_frame=time_frame_to_seconds(time_frame),
             min_timestamp=self.oldest_open_time(),
             max_timestamp=self.newest_open_time(),
@@ -79,6 +86,61 @@ class Getter:
     def newest_open_time(self) -> int:
         """The newest avaliable open time (integer timestamp)"""
         raise NotImplementedError
+
+    def _get_core(self, since: int, until: int) -> DF:
+        raise NotImplementedError
+
+    def get(self, **kwargs) -> DF:
+        """Solves the request, if that contains at least 2 of these 3
+        arguments: "since", "until", "number_samples".
+
+        Returns:
+            pd.core.frame.DataFrame: Requested klines range."""
+
+        since, until = self.sanitizer.sanitize(**kwargs)
+        _klines = self._get_core(since, until)
+
+        klines = _klines[_klines.Open_time <= until]
+        if self.settings.return_as_human_readable:
+            try:
+                klines.parse_datetime.to_human_readable(
+                    columns=["Open_time", "close_time"]
+                )
+            except TimeFormatError:
+                pass
+        return klines
+
+    def oldest(self, number_samples: int = 1) -> DF:
+        """Oldest <number_samples> klines avaliable.
+
+        Args:
+            number_samples (int, optional): Desired number of samples
+            (candles). Defaults to 1.
+
+        Returns:
+            pd.core.frame.DataFrame: klines, dataframe with extra methods.
+        """
+        klines = self.get(
+            number_samples=number_samples, since=self.oldest_open_time()
+        )
+        _len = min(number_samples, len(klines))
+        return klines[:_len]
+
+    def newest(self, number_samples: int = 1) -> DF:
+        """Newest <number_samples> klines avaliable.
+
+        Args:
+            number_samples (int, optional): Desired number of samples
+            (candles). Defaults to 1.
+
+        Returns:
+            pd.core.frame.DataFrame: klines, dataframe with extra methods.
+        """
+        klines = self.get(
+            number_samples=number_samples, until=self.newest_open_time()
+        )
+        _len = min(number_samples, len(klines))
+        return klines[-_len:]
 
 
 @pd.api.extensions.register_dataframe_accessor("apply_indicator")
