2b6182c17eb49f40f99f3f76520cfe6405fd6cb3
marcusmello <mandealista@gmail.com>
2021-04-30 12:16:44 -0300

    'fabric' appears to be a more suitable function name, given that the function returns an instance of a 'broker' object

--- app/src/lib/brokers/__init__.py
+++ app/src/lib/brokers/__init__.py
@@ -1,18 +1,18 @@
 """Entrypoint for instantiating a broker"""
 
 import sys
-
+from typing import List
 from . import base, binance, backtesting
 
 thismodule = sys.modules[__name__]
 
 
-def get_broker(broker_name: str) -> base.Broker:
-    """Returns a instatiate broker, if 'broker_name' matches an
+def fabric(broker: str) -> base.Broker:
+    """Returns an instatiate broker, if 'broker' matches an
     implemented broker
 
-    :param broker_name: The name of desired broker
-    :type broker_name: str
+    :param broker: The name of desired broker
+    :type broker: str
     :raises NotImplementedError: If broker name is not recognized
     as an implemented broker
     :return: A broker
@@ -20,17 +20,33 @@ def get_broker(broker_name: str) -> base.Broker:
     """
 
     try:
-        module = getattr(thismodule, broker_name.lower())
-        class_ = broker_name.capitalize()
+        module = getattr(thismodule, broker.lower())
+        class_ = broker.capitalize()
 
         return getattr(module, class_)()
 
     except AttributeError as err:
-        err_msg = "Not implented '{}' broker.".format(broker_name)
+        err_msg = "Not implented '{}' broker.".format(broker)
         raise NotImplementedError(err_msg) from err
 
 
-def get_backtesting_broker(operation):
-    return getattr(
-        backtesting, "{}Backtesting".format(operation.broker.capitalize())
-    )(operation)
+def backtesting_fabric(broker:str, assets:List[str]) -> base.Broker:
+    """[summary]
+
+    :param broker: [description]
+    :type broker: str
+    :param assets: [description]
+    :type assets: List[str]
+    :raises NotImplementedError: [description]
+    :return: [description]
+    :rtype: base.Broker
+    """
+
+    try:
+        real_broker = fabric(broker)
+        backtesting_broker = getattr(backtesting, "BackTestingBroker")
+        return backtesting_broker(real_broker, assets)
+
+    except AttributeError as err:
+        err_msg = "Not implented '{}' broker.".format(broker)
+        raise NotImplementedError(err_msg) from err
