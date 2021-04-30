069d2b83156a53f14109befb493ee652b7d59506
marcusmello <mandealista@gmail.com>
2021-04-30 15:25:07 -0300

    All done here

--- app/tests/lib/brokers/test_broker_fabric.py
+++ app/tests/lib/brokers/test_broker_fabric.py
@@ -5,18 +5,22 @@
 # pylint: disable=too-few-public-methods
 
 import pytest
-from src.lib.brokers import fabric
+from src.lib.brokers import Fabric
 
+@pytest.mark.parametrize("name", ["binance"])
+def test_for_a_implemented_broker(name:str):
+    broker = Fabric(broker=name).real()
+    assert broker.__class__.__name__ == name.capitalize()
 
-def test_implemented_broker():
-    broker_name = "binance"
-    broker = fabric(broker_name)
-    assert broker.__class__.__name__ == broker_name.capitalize()
-
-def test_for_a_not_implemented_broker():
+@pytest.mark.parametrize("name", ["monte_carlo"])
+def test_for_a_not_implemented_broker(name:str):
     with pytest.raises(NotImplementedError):
-        broker_name = "not_implemented"
-        fabric(broker_name)
+        Fabric(name)
 
-class TestGetBackTestingBroker:
-    pass
+@pytest.mark.parametrize("name, assets", [("binance", ["BTC", "NEO"])])
+def test_for_implemented_backtesting_broker(name:str, assets:list):
+    broker = Fabric(broker=name).backtesting(assets=assets)
+
+    assert broker.__class__.__name__ == 'BackTestingBroker'
+    assert broker.real_broker.__class__.__name__ == name.capitalize()
+    assert broker.assets == assets
