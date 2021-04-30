91f1833837a6cd57596f977ef4988dd8b69e88b7
marcusmello <mandealista@gmail.com>
2021-04-30 18:20:27 -0300

    All done here!

--- app/src/lib/marketdata/operators/indicators/simple_moving_average.py
+++ app/src/lib/marketdata/operators/indicators/simple_moving_average.py
@@ -1,15 +1,39 @@
-#from .base import price_columns
+"""Simple Moving Average"""
+
+# pylint: disable=no-name-in-module
+# pylint: disable=too-few-public-methods
+
+from pydantic import BaseModel
+
+class Setup(BaseModel):
+    """Setup of simple moving average indicator"""
+
+    price_metrics: str = "ohlc4"
+    number_samples: int = 7
+
 
 class SimpleMovingAverage:
+    """Calculates the average of a selected range of prices,
+    by the number of periods (number_samples) in that range.
+    """
+
     __slots__ = ["_klines"]
 
     def __init__(self, klines):
         self._klines = klines
 
-    def apply(self, setup):
-        self._klines.apply_indicator.trend.price_from_kline(
-            price_metrics=setup.price_metrics
-        )
+    def apply(self, setup:Setup=Setup()):
+        """SMA = (P1​ + P2 ​+ ... + Pn)/n
+
+            ​where:
+                Pn​ = the Price of an asset at period n
+                n = the number of total periods​ (number_samples)
+
+        :param setup: price_metrics and number_samples
+        :type setup: Setup, optional
+        """
+
+        self._klines.indicator.price.apply(price_metrics=setup.price_metrics)
         indicator_column = "SMA_{}".format(setup.number_samples)
 
         price_column = getattr(
