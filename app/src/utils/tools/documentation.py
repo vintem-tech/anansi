729f80467ac5e9031dbffe36e249399e1b5e2c62
marcusmello <mandealista@gmail.com>
2021-07-28 13:03:15 -0300

    refactor(QA): aplly some pylint suggestion, ignore others; rename and change the order of some methods and functions in order to improve clean code

--- app/src/utils/tools/documentation.py
+++ app/src/utils/tools/documentation.py
@@ -1,7 +1,11 @@
-from functools import partial, wraps
+# pylint:disable=missing-function-docstring
+# pylint:disable=missing-module-docstring
 
-class DocInherit(object):
-    """ Docstring inheriting method descriptor; the class itself is
+from functools import wraps
+
+
+class DocInherit:
+    """Docstring inheriting method descriptor; the class itself is
     also used as a decorator. See reference at:
     <http://code.activestate.com/recipes/576862/>
     """
@@ -10,32 +14,31 @@ class DocInherit(object):
         self.method = method
         self.name = method.__name__
 
-    def __get__(self, obj, cls):
-        if obj:
-            return self.get_with_inst(obj, cls)
-        else:
-            return self.get_no_inst(cls)
-
-    def get_with_inst(self, obj, cls):
+    def get_with_instance(self, obj, cls):
         overridden = getattr(super(cls, obj), self.name, None)
 
         @wraps(self.method, assigned=("__name__", "__module__"))
-        def f(*args, **kwargs):
+        def function_(*args, **kwargs):
             return self.method(obj, *args, **kwargs)
 
-        return self.use_parent_doc(f, overridden)
+        return self.use_parent_doc(function_, overridden)
 
-    def get_no_inst(self, cls):
+    def get_no_instance(self, cls):
         for parent in cls.__mro__[1:]:
             overridden = getattr(parent, self.name, None)
             if overridden:
                 break
 
         @wraps(self.method, assigned=("__name__", "__module__"))
-        def f(*args, **kwargs):
+        def function_(*args, **kwargs):
             return self.method(*args, **kwargs)
 
-        return self.use_parent_doc(f, overridden)
+        return self.use_parent_doc(function_, overridden)
+
+    def __get__(self, obj, cls):
+        if obj:
+            return self.get_with_instance(obj, cls)
+        return self.get_no_instance(cls)
 
     def use_parent_doc(self, func, source):
         if source is None:
