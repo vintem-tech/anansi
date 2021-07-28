573265b276701f5cbb176488d5961c5d048d043d
marcusmello <mandealista@gmail.com>
2021-07-28 13:15:53 -0300

    refactor(QA): aplly some pylint suggestion, ignore others; rename some variables for a cleaner code

--- app/src/utils/tools/serializers.py
+++ app/src/utils/tools/serializers.py
@@ -1,13 +1,18 @@
+# pylint:disable=missing-module-docstring
+# pylint:disable=missing-class-docstring
+# pylint:disable=missing-function-docstring
+
 import json
 from collections import namedtuple
 
+
 class Serialize:
-    def __init__(self, Target: object):
-        self.Target = Target
+    def __init__(self, target: object):
+        self.target = target
 
     def to_dict(self) -> dict:
         return json.loads(
-            json.dumps(self.Target, default=lambda o: o.__dict__, indent=0)
+            json.dumps(self.target, default=lambda o: o.__dict__, indent=0)
         )
 
     def to_json(self) -> str:
@@ -18,8 +23,8 @@ class Deserialize:
     def __init__(self, name="X"):
         self.name = name
 
-    def _json_object_hook(self, d):
-        return namedtuple(self.name, d.keys())(*d.values())
+    def _json_object_hook(self, dict_):
+        return namedtuple(self.name, dict_.keys())(*dict_.values())
 
     def from_json(self, json_in: str) -> object:
         return json.loads(json_in, object_hook=self._json_object_hook)
