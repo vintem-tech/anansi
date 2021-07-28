cdc86d37a844f81a2ed36c63caa95a2de77baa30
marcusmello <mandealista@gmail.com>
2021-07-28 13:10:06 -0300

    refactor(QA): create a class to group the hash scope's functions

--- app/src/utils/tools/hashes.py
+++ app/src/utils/tools/hashes.py
@@ -1,13 +1,27 @@
+# pylint: disable=no-name-in-module
+# pylint:disable=missing-function-docstring
+# pylint:disable=missing-module-docstring
+# pylint:disable=missing-class-docstring
+
 import hashlib
+
 from pydantic import BaseModel
 
 
-def hash_from_an_object(obj: BaseModel, hash_length: int = 8) -> str:
-    return hashlib.sha256(str(obj).encode("utf-8")).hexdigest()[:hash_length]
+class MakeHash:
+    @staticmethod
+    def sha256_from_an_object(obj: BaseModel, hash_length: int = 8) -> str:
+        return hashlib.sha256(str(obj).encode("utf-8")).hexdigest()[
+            :hash_length
+        ]
 
-def another_hash_from_an_object(obj: BaseModel, hash_length: int = 8) -> str:
-    return int(hashlib.md5(str(obj).encode("utf-8")).hexdigest(), hash_length)
+    @staticmethod
+    def md5_from_an_object(obj: BaseModel, hash_length: int = 8) -> str:
+        return int(
+            hashlib.md5(str(obj).encode("utf-8")).hexdigest(), hash_length
+        )
 
-def string_hash_from_string(input_string:str, hash_len=8):
-    _hash = hashlib.md5(input_string.encode("utf-8")).hexdigest()
-    return _hash[:hash_len]
+    @staticmethod
+    def md5_from_string(input_string: str, hash_len=8):
+        _hash = hashlib.md5(input_string.encode("utf-8")).hexdigest()
+        return _hash[:hash_len]
