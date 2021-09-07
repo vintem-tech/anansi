c76d383189c07a69490de80c091fec592634b489
marcusmello <mandealista@gmail.com>
2021-09-07 14:55:59 -0300

    refactor(web): rename package 'pages' to 'routes'

--- backend/src/web/routes/home.py
+++ backend/src/web/routes/home.py
@@ -1,12 +1,12 @@
-from .router import app, templates, HTMLResponse, Request
+from .router import web_app, templates, HTMLResponse, Request
 
-@app.get("/", response_class=HTMLResponse)
+@web_app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse(
         "home.html", {"request": request}
     )
 
-@app.get("/home", response_class=HTMLResponse)
+@web_app.get("/home", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
         "home.html", {"request": request}
