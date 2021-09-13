e91e5563de39b314ca7da29fdafbea004c702622
marcusmello <mandealista@gmail.com>
2021-09-13 20:22:33 -0300

    refactor(database): get the database connection args from the system configuration dynamically

--- backend/src/utils/databases/sql/models/__init__.py
+++ backend/src/utils/databases/sql/models/__init__.py
@@ -2,12 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./anansi.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from .....config import SystemSettings
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+from urllib.parse import urlparse
+
+sql_adapter = SystemSettings().relational_database_adapter
+
+#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}:{}/{}"
+db_url = SQLALCHEMY_DATABASE_URL.format("anansi_user", "!*_anansi_pass_123", "postgres", 5432, "anansi_postgres")
+
+#engine = create_engine(**sql_adapter.connection_args())
+engine = create_engine(url=db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
