8aba07a119bffd59c48569dd50602fb86dfdb74f
marcusmello <mandealista@gmail.com>
2021-09-22 16:00:30 -0300

    refactor(proxy): add 'HOST_PORT' in envsub | adjust 'ARCHITECTURE_TYPE' variable | delete templates after variables replacement

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 set -eu
-
-envsubst '${HOSTNAME}' < /'${ARCHITECTURE_TYPE}'.default.conf.template > /etc/nginx/conf.d/default.conf
-
+envsubst '${HOST_NAME}:${HOST_PORT}' < /$ARCHITECTURE_TYPE.template > /etc/nginx/conf.d/default.conf
+rm -rf microservices.template monolithic.template
 exec "$@"
