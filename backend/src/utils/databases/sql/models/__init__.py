4cdb6b8f07b394b3a6c8b6026b9936f8bf349678
marcusmello <mandealista@gmail.com>
2021-09-22 16:19:22 -0300

    fix(databases): delete useless stuf and back to 'connection args' due to database url fix

--- backend/src/utils/databases/sql/models/__init__.py
+++ backend/src/utils/databases/sql/models/__init__.py
@@ -4,16 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 from .....config import SystemSettings
 
-from urllib.parse import urlparse
-
 sql_adapter = SystemSettings().relational_database_adapter
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}:{}/{}"
-db_url = SQLALCHEMY_DATABASE_URL.format("anansi_user", "!*_anansi_pass_123", "postgres", 5432, "anansi_postgres")
-
-#engine = create_engine(**sql_adapter.connection_args())
-engine = create_engine(url=db_url)
+engine = create_engine(**sql_adapter.connection_args())
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
