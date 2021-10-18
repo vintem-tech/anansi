3fd8039534217ab3430a874795c9f08d2c4b8069
marcusmello <mandealista@gmail.com>
2021-10-18 20:02:21 -0300

    fix(core): fix 'settings' import path

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -3,13 +3,10 @@ from typing import Any, Union
 
 from jose import jwt
 from passlib.context import CryptContext
-
-from app.core.config import settings
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+from src.core.config import settings
 
 ALGORITHM = "HS256"
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def create_access_token(
