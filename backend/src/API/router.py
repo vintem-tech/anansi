934a89b0d58fdb1eb074b45a994952acc74bbc32
marcusmello <mandealista@gmail.com>
2021-10-18 19:58:33 -0300

    feat(API): add 'login' route

--- backend/src/API/router.py
+++ backend/src/API/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .api_v1.endpoints import items, users
+from .api_v1.endpoints import items, users, login
 
 api = APIRouter()
 
 api.include_router(items.endpoint, prefix="/items")
 api.include_router(users.endpoint, prefix="/users")
+api.include_router(login.endpoint, prefix="/login")
