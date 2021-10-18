0ac3dadff467e31f53e48259acf34f503f4e5bbc
marcusmello <mandealista@gmail.com>
2021-10-18 19:59:48 -0300

    refactor(config): move 'config' to core

--- backend/src/core/config/__init__.py
+++ backend/src/core/config/__init__.py
@@ -25,10 +25,21 @@ class SystemSettings(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
+    SECRET_KEY: str = env.str(
+        "SECRET_KEY",
+        default="ca809d720daa1acc6e63d8f58bfd4006bd8f6614c272543c17ec1598f90b2158",
+    )
+    FIRST_SUPERUSER:str = env.str("FIRST_SUPERUSER", default="anansi@email.com")
+    FIRST_SUPERUSER_PASSWORD:str = env.str("FIRST_SUPERUSER_PASSWORD", default="12345")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int(
+        "ACCESS_TOKEN_EXPIRE_MINUTES", default=15
+    )
     date_time = DateTimeSettings()
     sql_debug: bool = env.bool("SQL_DEBBUG", default=False)
     relational_database_adapter: BaseModel = get_relational_database_settings(
         provider=env.str("RELATIONAL_DATABASE_PROVIDER", default="sqlite")
     )
     time_series_storage: BaseModel = InfluxDbSettings()
-    API_prefix_version: str = env.str("API_PREFIX_VERSION", default="/api/v1")
+    API_PREFIX_VERSION: str = env.str("API_PREFIX_VERSION", default="/api/v1")
+
+settings = SystemSettings()
