7f6c6c1d21623970b2a8c89845def26146b0e958
marcusmello <mandealista@gmail.com>
2021-10-18 19:57:49 -0300

    refactor(API): move deps out of 'v1' subpackage to main package

--- backend/src/API/deps.py
+++ backend/src/API/deps.py
@@ -5,14 +5,14 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
-
-from app import crud, models, schemas
-from app.core import security
-from app.core.config import settings
-from app.db.session import SessionLocal
+from src.core import security
+from src.core.config import settings
+from src.utils.databases.sql.core.session import SessionLocal
+from src.utils import schemas
+from src.utils.databases.sql import crud, models
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+    tokenUrl=f"{settings.API_PREFIX_VERSION}/login/access-token"
 )
 
 
