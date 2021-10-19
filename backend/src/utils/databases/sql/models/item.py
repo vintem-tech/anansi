8d1878e8a59de98bf4c4d30053ff68188d7201fa
marcusmello <mandealista@gmail.com>
2021-10-19 20:21:17 -0300

    refactor(models): refactor the 'Item' model due to 'Base' class refactoring

--- backend/src/utils/databases/sql/models/item.py
+++ backend/src/utils/databases/sql/models/item.py
@@ -3,18 +3,19 @@
 # pylint:disable=too-few-public-methods
 
 
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from src.utils.databases.sql.core.base_class import Base
 
-from . import Base
+if TYPE_CHECKING:
+    from .user import User  # noqa: F401
 
 
 class Item(Base):
-    __tablename__ = "items"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
+    owner_id = Column(Integer, ForeignKey("user.id"))
     owner = relationship("User", back_populates="items")
