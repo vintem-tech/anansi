ea0983c003acb6f1265b32a708c150f041174d06
marcusmello <mandealista@gmail.com>
2021-10-19 20:55:56 -0300

    refactor(schemas): create new useful user schemas

--- backend/src/utils/schemas/user.py
+++ backend/src/utils/schemas/user.py
@@ -3,25 +3,42 @@
 # pylint:disable=missing-class-docstring
 # pylint:disable=too-few-public-methods
 
-from typing import List
+from typing import Optional
 
-from pydantic import BaseModel
-
-from .item import Item
+from pydantic import BaseModel, EmailStr
 
 
+# Shared properties
 class UserBase(BaseModel):
-    email: str
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    full_name: Optional[str] = None
 
 
+# Properties to receive via API on creation
 class UserCreate(UserBase):
+    email: EmailStr
     password: str
 
 
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
 
     class Config:
         orm_mode = True
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    pass
+
+
+# Additional properties stored in DB
+class UserInDB(UserInDBBase):
+    hashed_password: str
