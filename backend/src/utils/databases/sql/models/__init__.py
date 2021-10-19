1713dabbdba92de1edc9d0a63fa7ba471acab086
marcusmello <mandealista@gmail.com>
2021-10-19 20:17:51 -0300

    refactor(models): import the class models from the respective modules

--- backend/src/utils/databases/sql/models/__init__.py
+++ backend/src/utils/databases/sql/models/__init__.py
@@ -1,11 +1,2 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-from .....config import SystemSettings
-
-sql_adapter = SystemSettings().relational_database_adapter
-
-engine = create_engine(**sql_adapter.connection_args())
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+from .item import Item
+from .user import User
