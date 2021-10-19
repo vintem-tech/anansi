0fd38981c32df8eb116dce3b61bd7728f9ca4b0b
marcusmello <mandealista@gmail.com>
2021-10-19 20:10:57 -0300

    refactor(crud): move useless module to backup

--- backend/src/utils/databases/sql/crud/single_ticker_monitoring.py
+++
@@ -1,5 +0,0 @@
-import json
-from typing import List, Union
-from src.utils import schemas
-from src.utils.databases.sql.models import SingleTickerMonitoring
-from pony.orm import db_session, select
