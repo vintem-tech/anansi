447182ecca8c7d7cbd91400484813c1cfd7df7e0
marcusmello <mandealista@gmail.com>
2021-10-25 19:39:51 -0300

    feat(models): create first trading models

---
+++ backend/src/utils/databases/sql/models/trading.py
@@ -0,0 +1,24 @@
+# pylint:disable=missing-module-docstring
+# pylint:disable=missing-class-docstring
+# pylint:disable=too-few-public-methods
+
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, ForeignKey, Integer, String, ARRAY, JSON
+from sqlalchemy.orm import relationship
+from src.utils.databases.sql.core.base_class import Base
+
+if TYPE_CHECKING:
+    from .user import User  # noqa: F401
+
+
+class Trader(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    broker = Column(String, index=True)
+    tickers = Column(ARRAY)
+    mode = Column(String)
+    classifier = Column(String)
+    classifier_setup = Column(JSON)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner = relationship("User", back_populates="traders")
