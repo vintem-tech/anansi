ad62e06dbc33ce967cefe3de36e54d9584330cd8
marcusmello <mandealista@gmail.com>
2021-10-26 18:31:36 -0300

    refactor(API): add trading endpoints to the router

--- backend/src/API/router.py
+++ backend/src/API/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from .api_v1.endpoints import items, users, login
+from .api_v1.endpoints import items, users, login, trading
 
 api = APIRouter()
 
 api.include_router(items.endpoint, prefix="/items")
 api.include_router(users.endpoint, prefix="/users")
 api.include_router(login.endpoint, prefix="/login")
+api.include_router(trading.endpoint, prefix="/trading")
