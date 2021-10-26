731792ed083351b7253f96e7fff1d6d889f14c49
marcusmello <mandealista@gmail.com>
2021-10-26 18:41:25 -0300

    feat(schemas): add the 'Traders' schemas to the init

--- backend/src/utils/schemas/__init__.py
+++ backend/src/utils/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .item import Item, ItemCreate, ItemInDB, ItemUpdate
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
+from .trading import Trader, TraderCreate, TraderUpdate, TraderInDB
