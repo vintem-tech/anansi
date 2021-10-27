931bc8d4b40788a166110a46d63b803d55fd97de
marcusmello <mandealista@gmail.com>
2021-10-27 20:45:55 -0300

    refactor(settings): add 'TELEGRAM_BOT_TOKEN' variable

--- backend/src/core/config/__init__.py
+++ backend/src/core/config/__init__.py
@@ -29,9 +29,17 @@ class SystemSettings(BaseModel):
         "SECRET_KEY",
         default="ca809d720daa1acc6e63d8f58bfd4006bd8f6614c272543c17ec1598f90b2158",
     )
-    FIRST_SUPERUSER:str = env.str("FIRST_SUPERUSER", default="anansi@email.com")
-    FIRST_SUPERUSER_PASSWORD:str = env.str("FIRST_SUPERUSER_PASSWORD", default="9cjf-034erhnfivdk")
-    USERS_OPEN_REGISTRATION:bool = False
+    FIRST_SUPERUSER: str = env.str(
+        "FIRST_SUPERUSER", default="anansi@email.com"
+    )
+    FIRST_SUPERUSER_PASSWORD: str = env.str(
+        "FIRST_SUPERUSER_PASSWORD", default="12345"
+    )
+    TELEGRAM_BOT_TOKEN: str = env.str(
+        "TELEGRAM_BOT_TOKEN",
+        default="0123456789:ageRPkvuPXBcChgrZSnz9rF9W23Gv7hthuT",
+    )
+    USERS_OPEN_REGISTRATION: bool = False
     ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int(
         "ACCESS_TOKEN_EXPIRE_MINUTES", default=120
     )
@@ -42,5 +50,6 @@ class SystemSettings(BaseModel):
     )
     time_series_storage: BaseModel = InfluxDbSettings()
     API_PREFIX_VERSION: str = env.str("API_PREFIX_VERSION", default="/api/v1")
+
 
 settings = SystemSettings()
