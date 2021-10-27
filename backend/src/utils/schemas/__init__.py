c1e75a2061cc6f22fe5fd8d0b3e90db518015e76
marcusmello <mandealista@gmail.com>
2021-10-27 20:51:07 -0300

    refactor(schemas): import TelegramChatsIds class

--- backend/src/utils/schemas/__init__.py
+++ backend/src/utils/schemas/__init__.py
@@ -2,3 +2,4 @@ from .item import Item, ItemCreate, ItemInDB, ItemUpdate
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
 from .trading import Trader, TraderCreate, TraderUpdate, TraderInDB
+from .notifiers import TelegramChatsIds
