058b387289cff657c7142a76405b81a859eef4b6
marcusmello <mandealista@gmail.com>
2021-10-28 12:00:43 -0300

    refactor(project): rebuild using PONY ORM

--- backend/src/utils/schemas/backtesting.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=no-name-in-module
-# pylint: disable=no-self-argument
-# pylint: disable=too-few-public-methods
-
-# from typing import Dict, Optional, Union, List
-
-# from pydantic import BaseModel, EmailStr
-
