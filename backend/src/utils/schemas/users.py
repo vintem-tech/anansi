b8ec61130d55e1b89837b660e14b2b93bb0a3fc9
marcusmello <mandealista@gmail.com>
2021-10-28 19:59:15 -0300

    refactor(schemas): recreate User schemas

--- backend/src/utils/schemas/users.py
+++ backend/src/utils/schemas/users.py
@@ -6,9 +6,9 @@
 from typing import Optional
 
 from pydantic import BaseModel, EmailStr
-from .notifiers import TelegramChatsIds
 
-# Shared properties
+from src.utils.schemas.notifiers import TelegramChatsIds
+
 class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
@@ -16,31 +16,6 @@ class UserBase(BaseModel):
     full_name: Optional[str] = None
     telegram_chats_ids:TelegramChatsIds = TelegramChatsIds()
 
-
-
-# Properties to receive via API on creation
 class UserCreate(UserBase):
     email: EmailStr
     password: str
-
-
-# Properties to receive via API on update
-class UserUpdate(UserBase):
-    password: Optional[str] = None
-
-
-class UserInDBBase(UserBase):
-    id: Optional[int] = None
-
-    class Config:
-        orm_mode = True
-
-
-# Additional properties to return via API
-class User(UserInDBBase):
-    pass
-
-
-# Additional properties stored in DB
-class UserInDB(UserInDBBase):
-    hashed_password: str
