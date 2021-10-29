8315ad27d7191db0000b4f5d106ddbc6d93a376b
marcusmello <mandealista@gmail.com>
2021-10-29 10:21:47 -0300

    refactor(API): cleanup the router and add the users endpoint

--- backend/src/API/router.py
+++ backend/src/API/router.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter
 
-#from .api_v1.endpoints import items, users, login, trading
+from .api_v1.endpoints import users
 
 api = APIRouter()
 
-#api.include_router(items.endpoint, prefix="/items")
-#api.include_router(users.endpoint, prefix="/users")
-#api.include_router(login.endpoint, prefix="/login")
-#api.include_router(trading.endpoint, prefix="/trading")
+api.include_router(users.endpoint, prefix="/users")
