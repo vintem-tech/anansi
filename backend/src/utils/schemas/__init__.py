cc09a412267ccc897a7f749cccf5ddf95b6eaa9a
marcusmello <mandealista@gmail.com>
2021-10-29 10:24:21 -0300

    refactor(schemas): import the useful schemas to the init

--- backend/src/utils/schemas/__init__.py
+++ backend/src/utils/schemas/__init__.py
@@ -1,5 +1,1 @@
-from .item import Item, ItemCreate, ItemInDB, ItemUpdate
-from .token import Token, TokenPayload
-#from .user import User, UserCreate, UserInDB, UserUpdate
-from .trading import Trader, TraderCreate, TraderUpdate, TraderInDB
-from .notifiers import TelegramChatsIds
+from .users import UserCreate, UserBase
