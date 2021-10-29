8e0dedabb9d70c853ce0ed22a83766c9f46df15d
marcusmello <mandealista@gmail.com>
2021-10-29 18:56:02 -0300

    refactor(crud): add hash password to the User model

--- backend/src/utils/databases/sql/crud/users.py
+++ backend/src/utils/databases/sql/crud/users.py
@@ -1,13 +1,22 @@
-from src.utils.databases.sql.core.pony import safety_commit, db_session, commit
-from src.utils.databases.sql.models import users
-from src.utils.schemas.users import UserCreate
+from src.utils.databases.sql.core.pony import safety_commit, db_session
+from src.utils.databases.sql.models.users import User
+from src.utils.schemas.users import UserCreate, UserReturn
+from src.core.security import get_password_hash
 
 class CrudUser:
     
-    def create(self, user_create: UserCreate):
+    def create(self, user_create: UserCreate) -> UserReturn:
+        password_in = user_create.password
+        hash_password = get_password_hash(password_in)
+        user_create.password = hash_password
+
         with db_session:
-            user = users.User(**user_create.dict())
+            user = User(**user_create.dict())
             safety_commit()
-        return user_create
+        
+        user_return = user.to_dict()
+        user_return.pop("password")
+        return UserReturn(**user_return)
+
 
 user = CrudUser()
