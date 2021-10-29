96ae047af17f8e8fc3d9d2d21c0055a4087ecc09
marcusmello <mandealista@gmail.com>
2021-10-29 18:57:27 -0300

    refactor(schemas): create other useful User schemas

--- backend/src/utils/schemas/users.py
+++ backend/src/utils/schemas/users.py
@@ -9,13 +9,25 @@ from pydantic import BaseModel, EmailStr
 
 from src.utils.schemas.notifiers import TelegramChatsIds
 
+# Shared properties
 class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
     is_superuser: bool = False
-    full_name: Optional[str] = None
+    full_name: Optional[str] = str()
     telegram_chats_ids:TelegramChatsIds = TelegramChatsIds()
 
+
+# Makes email and password required (receive via API on creation)
 class UserCreate(UserBase):
     email: EmailStr
     password: str
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[str] = str()
+
+
+# Properties to return via API
+class UserReturn(UserBase):
+    id: int
