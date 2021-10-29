1b7edc06944ab7562c1c3d526a5670d1fc6a8f4d
marcusmello <mandealista@gmail.com>
2021-10-29 10:21:03 -0300

    feat(API): create the 'create user' initial endpoint

---
+++ backend/src/API/api_v1/endpoints/users.py
@@ -0,0 +1,11 @@
+from typing import Any, List
+
+from fastapi import APIRouter
+from src.utils import schemas
+from src.utils.databases.sql.crud import user
+
+endpoint = APIRouter()
+
+@endpoint.post("/", response_model=schemas.UserBase)
+def create_user(user_in:schemas.UserCreate):
+    return user.create(user_in)
