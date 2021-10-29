074f1c024fdf46b0fb84764e90f498ea0d118a12
marcusmello <mandealista@gmail.com>
2021-10-29 19:00:39 -0300

    feat(crud): add read to the User crud

--- backend/src/utils/databases/sql/crud/users.py
+++ backend/src/utils/databases/sql/crud/users.py
@@ -1,10 +1,10 @@
-from src.utils.databases.sql.core.pony import safety_commit, db_session
+from src.core.security import get_password_hash
+from src.utils.databases.sql.core.pony import db_session, safety_commit
 from src.utils.databases.sql.models.users import User
 from src.utils.schemas.users import UserCreate, UserReturn
-from src.core.security import get_password_hash
+
 
 class CrudUser:
-    
     def create(self, user_create: UserCreate) -> UserReturn:
         password_in = user_create.password
         hash_password = get_password_hash(password_in)
@@ -13,10 +13,18 @@ class CrudUser:
         with db_session:
             user = User(**user_create.dict())
             safety_commit()
-        
+
         user_return = user.to_dict()
         user_return.pop("password")
         return UserReturn(**user_return)
+
+    def read_by_id(self, id: int) -> UserReturn:
+        try:
+            with db_session:
+                user_return = User.get(id=id)
+            return UserReturn(**user_return.to_dict())
+        except (KeyError, AttributeError):
+            return None
 
 
 user = CrudUser()
