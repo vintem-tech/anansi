8713d9cf80403fbcc151d7372af398fda2656bcd
marcusmello <mandealista@gmail.com>
2021-10-30 20:12:15 -0300

    refactor(main): include populate_database routine | delete useless path

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 
 from src.API.router import api
 from src.web.routes import router
+from src.services import start
 
 app = FastAPI()
 app.include_router(api, prefix="/api/v1")
 app.mount("", router.web_app)
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+start.populate_database()
