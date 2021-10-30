f0b26b596e4d2a272c3b844bcc851dc5442dfdb7
marcusmello <mandealista@gmail.com>
2021-10-30 20:09:42 -0300

    style(models): aplly black formatting

--- backend/src/utils/databases/sql/models/__init__.py
+++ backend/src/utils/databases/sql/models/__init__.py
@@ -1,6 +1,6 @@
-#from .item import Item
-from .users import User
-from .trading import Trader
 from src.utils.databases.sql.core.pony import db
+
+from .trading import Trader
+from .users import User
 
 db.generate_mapping(create_tables=True)
