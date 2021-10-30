75fcaf2dbd9910eca8fbbd31684e26d6e8b7432f
marcusmello <mandealista@gmail.com>
2021-10-30 07:55:29 -0300

    refactor(API): add a email validation to avoid multiple user with the same email

--- backend/src/API/api_v1/endpoints/users.py
+++ backend/src/API/api_v1/endpoints/users.py
@@ -6,13 +6,21 @@ from src.utils.databases.sql.crud import user
 
 endpoint = APIRouter()
 
+
 @endpoint.post("/", response_model=schemas.UserReturn)
-def create_user(user_in:schemas.UserCreate):
+def create_user(user_in: schemas.UserCreate):
+    user_in_db = user.read_by_email(email=user_in.email)
+    if user_in_db:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already in use. Login or try a different one.",
+        )
     return user.create(user_in)
 
+
 @endpoint.get("/{id}", response_model=schemas.UserReturn)
-def read_user_by_id(id:int):
+def read_user_by_id(id: int):
     user_in_db = user.read_by_id(id)
     if not user_in_db:
-        raise HTTPException(status_code=400, detail="User does not exist") 
+        raise HTTPException(status_code=400, detail="User does not exist")
     return user_in_db
