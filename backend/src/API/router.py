454806c45e4d87a273e4d71f117d112cf12aa05a
marcusmello <mandealista@gmail.com>
2021-10-30 15:55:48 -0300

    feat(API): register the trading endpoint in router

--- backend/src/API/router.py
+++ backend/src/API/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .api_v1.endpoints import users
+from .api_v1.endpoints import users, trading
 
 api = APIRouter()
 
 api.include_router(users.endpoint, prefix="/users")
+api.include_router(trading.endpoint, prefix='/trading')
