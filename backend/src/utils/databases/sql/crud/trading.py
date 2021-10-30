c19e4d3eee4b4c02ecf24c2475f30d7b2d3b9429
marcusmello <mandealista@gmail.com>
2021-10-30 13:18:55 -0300

    feat(crud): add 'create by owner id/email' methods

--- backend/src/utils/databases/sql/crud/trading.py
+++ backend/src/utils/databases/sql/crud/trading.py
@@ -2,21 +2,26 @@ from src.utils.databases.sql.core.pony import db_session, safety_commit
 from src.utils.databases.sql.models.trading import Trader
 from src.utils.databases.sql.models.users import User
 from src.utils.schemas import TraderCreate, TraderReturn
-
+from pydantic import EmailStr
 
 class CrudTrader:
-    def create_by_owner(self, trader_create:TraderCreate, owner: User):
+    def create_by_owner(self, trader_create: TraderCreate, owner: User):
         with db_session:
             trader = Trader(**trader_create, owner=owner)
             safety_commit()
-        
-        trader_return = trader.to_dict()
+            trader_return = trader.to_dict()
+
         return TraderReturn(**trader_return)
 
-    def create_by_owner_id(self):
-        pass
+    def create_by_owner_id(self, trader_create: TraderCreate, id=int):
+        with db_session:
+            user_return = User.get(id=id)
+            return self.create_by_owner(trader_create, user_return)
 
-    def create_by_owner_email(self):
-        pass
+    def create_by_owner_email(self, trader_create: TraderCreate, email:EmailStr):
+        with db_session:
+            user_return = User.get(email=email)
+            return self.create_by_owner(trader_create, user_return)
+
 
 trader = CrudTrader()
