1203773e5f874e7a0782b9fe844b28383dfc8086
marcusmello <mandealista@gmail.com>
2021-10-30 07:42:22 -0300

    feat(API): create the 'read user by id' endpoint

--- backend/src/API/api_v1/endpoints/users.py
+++ backend/src/API/api_v1/endpoints/users.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.utils import schemas
 from src.utils.databases.sql.crud import user
 
@@ -9,3 +9,10 @@ endpoint = APIRouter()
 @endpoint.post("/", response_model=schemas.UserReturn)
 def create_user(user_in:schemas.UserCreate):
     return user.create(user_in)
+
+@endpoint.get("/{id}", response_model=schemas.UserReturn)
+def read_user_by_id(id:int):
+    user_in_db = user.read_by_id(id)
+    if not user_in_db:
+        raise HTTPException(status_code=400, detail="User does not exist") 
+    return user_in_db
