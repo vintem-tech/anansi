2e0f576548cb2c641d165efeba8cbfb82f401f25
marcusmello <mandealista@gmail.com>
2021-11-01 18:36:02 -0300

    feat(crud): create 'read real traders' method

--- backend/src/utils/databases/sql/crud/trading.py
+++ backend/src/utils/databases/sql/crud/trading.py
@@ -1,8 +1,9 @@
+from pydantic import EmailStr
 from src.utils.databases.sql.core.pony import db_session, safety_commit
 from src.utils.databases.sql.models.trading import Trader
 from src.utils.databases.sql.models.users import User
-from src.utils.schemas import TraderCreate, TraderReturn
-from pydantic import EmailStr
+from src.utils.schemas import TraderCreate, TraderReturn, modes
+
 
 class CrudTrader:
     def create_by_owner(self, trader_create: TraderCreate, owner: User):
@@ -18,10 +19,16 @@ class CrudTrader:
             user_return = User.get(id=id)
             return self.create_by_owner(trader_create, user_return)
 
-    def create_by_owner_email(self, trader_create: TraderCreate, email:EmailStr):
+    def create_by_owner_email(
+        self, trader_create: TraderCreate, email: EmailStr
+    ):
         with db_session:
             user_return = User.get(email=email)
             return self.create_by_owner(trader_create, user_return)
+
+    def read_real_traders(self):
+        traders = Trader.select().filter(lambda t: t.mode == modes.real)
+        return [TraderReturn(**trader.to_dict()) for trader in traders]
 
 
 trader = CrudTrader()
