33528dd1fd2db10ef4888198b0b284695cee81f4
marcusmello <mandealista@gmail.com>
2021-11-09 13:29:14 -0300

    feat(API): add login route

--- backend/src/API/router.py
+++ backend/src/API/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .api_v1.endpoints import users, trading
+from .api_v1.endpoints import login, trading, users
 
 api = APIRouter()
 
 api.include_router(users.endpoint, prefix="/users")
-api.include_router(trading.endpoint, prefix='/trading')
+api.include_router(trading.endpoint, prefix="/trading")
+api.include_router(login.endpoint, prefix="/login")
